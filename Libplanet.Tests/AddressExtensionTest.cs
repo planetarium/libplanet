3592bb48b4a116bece2f8ace8fd763138027ffd7
Swen Mun <longfinfunnel@gmail.com>
2018-11-21 22:16:20 +0900

    Add ToAddress to PublicKey (by extension)

---
+++ Libplanet.Tests/AddressExtensionTest.cs
@@ -0,0 +1,21 @@
+using Libplanet.Crypto;
+using Xunit;
+
+namespace Libplanet.Tests
+{
+    public class AddressExtensionTest
+    {
+        [Fact]
+        public void CanGetAddress()
+        {
+            PublicKey key = PublicKey.FromBytes(ByteUtil.ParseHex(
+                "03438b935389a7ebf838b3ae4125bd28506aa2dd457f20afc843729d3e7d60d728"));
+            Assert.Equal(
+                new Address(
+                    ByteUtil.ParseHex(
+                        "d41fadf61badf5be2de60e9fc3230c0a8a4390f0")),
+                key.ToAddress()
+            );
+        }
+    }
+}
