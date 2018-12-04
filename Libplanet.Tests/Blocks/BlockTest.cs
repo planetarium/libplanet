62a03c3eaea5124a05cf3aaf2bc77e198a47c41a
Swen Mun <longfinfunnel@gmail.com>
2018-12-04 17:50:09 +0900

    Refactor BlockTest (to reuse block fixture.)

--- Libplanet.Tests/Blocks/BlockTest.cs
+++ Libplanet.Tests/Blocks/BlockTest.cs
@@ -9,76 +9,74 @@ using Xunit;
 
 namespace Libplanet.Tests.Blocks
 {
-    public class BlockTest
+    public class BlockTest : IClassFixture<BlockFixture>
     {
+        private BlockFixture _fx;
+
+        public BlockTest(BlockFixture fixture) => _fx = fixture;
+
         [Fact]
         public void CanMine()
         {
-            Block<Action> genesis = MineGenesis();
-            Assert.Equal(0, genesis.Index);
-            Assert.Equal(0, genesis.Difficulty);
-            Assert.Null(genesis.PreviousHash);
-            Assert.Equal(new DateTime(2018, 11, 29), genesis.Timestamp);
+            Assert.Equal(0, _fx.Genesis.Index);
+            Assert.Equal(0, _fx.Genesis.Difficulty);
+            Assert.Null(_fx.Genesis.PreviousHash);
+            Assert.Equal(new DateTime(2018, 11, 29), _fx.Genesis.Timestamp);
             Assert.Equal(
                 new Address(ByteUtil.ParseHex("21744f4f08db23e044178dafb8273aeb5ebe6644")),
-                genesis.RewardBeneficiary);
-            Assert.Equal(new Nonce(new byte[] { 0x01, 0x00, 0x00, 0x00 }), genesis.Nonce);
+                _fx.Genesis.RewardBeneficiary);
+            Assert.Equal(new Nonce(new byte[] { 0x01, 0x00, 0x00, 0x00 }), _fx.Genesis.Nonce);
             Assert.Equal(
                 new HashDigest<SHA256>(
                     ByteUtil.ParseHex(
                         "b23717ecdf0b9c83bcc92f5675f827c1ee6808887bf1ce14f0322318174e1007"
                     )
                 ),
-                genesis.Hash
+                _fx.Genesis.Hash
             );
 
-            Block<Action> next = MineNext(genesis);
+            Block<Action> next = TestUtils.MineNext(_fx.Genesis);
 
-            Assert.Equal(1, next.Index);
-            Assert.Equal(1, next.Difficulty);
-            Assert.Equal(genesis.Hash, next.PreviousHash);
-            Assert.Equal(new DateTime(2018, 11, 30), next.Timestamp);
+            Assert.Equal(1, _fx.Next.Index);
+            Assert.Equal(1, _fx.Next.Difficulty);
+            Assert.Equal(_fx.Genesis.Hash, _fx.Next.PreviousHash);
+            Assert.Equal(new DateTime(2018, 11, 30), _fx.Next.Timestamp);
             Assert.Equal(
                 new Address(ByteUtil.ParseHex("21744f4f08db23e044178dafb8273aeb5ebe6644")),
-                next.RewardBeneficiary);
+                _fx.Next.RewardBeneficiary);
         }
 
         [Fact]
         public void CanBencode()
         {
-            Block<Action> genesis = MineGenesis();
-
             byte[] expected = ByteUtil.ParseHex(
                 "6431303a646966666963756c7479693065343a6861736833323ab23717ecdf0b9c83bcc92f5675f827c1ee6808887bf1ce14f0322318174e1007353a696e646578693065353a6e6f6e6365343a0100000031383a7265776172645f62656e656669636961727932303a21744f4f08db23e044178dafb8273aeb5ebe6644393a74696d657374616d7032373a323031382d31312d32395430303a30303a30302e3030303030305a31323a7472616e73616374696f6e736c6565"
             );
 
-            Assert.Equal(expected, genesis.Bencode(true, true));
+            Assert.Equal(expected, _fx.Genesis.Bencode(true, true));
         }
 
         [Fact]
         public void CanValidate()
         {
-            Block<Action> genesis = MineGenesis();
-            Block<Action> next = MineNext(genesis);
+            Block<Action> genesis = TestUtils.MineGenesis<Action>();
+            Block<Action> next = TestUtils.MineNext(genesis);
 
-            genesis.Validate();
-            next.Validate();
+            _fx.Genesis.Validate();
+            _fx.Next.Validate();
         }
 
         [Fact]
         public void CanDetectInvalidNonce()
         {
-            Block<Action> genesis = MineGenesis();
-            Block<Action> next = MineNext(genesis);
-
             var invalidBlock = new Block<Action>(
-                index: next.Index,
-                difficulty: next.Difficulty,
+                index: _fx.Next.Index,
+                difficulty: _fx.Next.Difficulty,
                 nonce: new Nonce(new byte[] { 0x00 }),
-                rewardBeneficiary: next.RewardBeneficiary,
-                previousHash: next.PreviousHash,
-                timestamp: next.Timestamp,
-                transactions: next.Transactions
+                rewardBeneficiary: _fx.Next.RewardBeneficiary,
+                previousHash: _fx.Next.PreviousHash,
+                timestamp: _fx.Next.Timestamp,
+                transactions: _fx.Next.Transactions
             );
 
             Assert.Throws<InvalidBlockNonceException>(() => { invalidBlock.Validate(); });
@@ -103,28 +101,25 @@ namespace Libplanet.Tests.Blocks
         [Fact]
         public void CanDetectInvalidDifficulty()
         {
-            Block<Action> genesis = MineGenesis();
-            Block<Action> next = MineNext(genesis);
-
             var invalidGenesis = new Block<Action>(
-                index: genesis.Index,
+                index: _fx.Genesis.Index,
                 difficulty: 1, // invalid
-                nonce: genesis.Nonce,
-                rewardBeneficiary: genesis.RewardBeneficiary,
-                previousHash: genesis.PreviousHash,
-                timestamp: genesis.Timestamp,
-                transactions: genesis.Transactions
+                nonce: _fx.Genesis.Nonce,
+                rewardBeneficiary: _fx.Genesis.RewardBeneficiary,
+                previousHash: _fx.Genesis.PreviousHash,
+                timestamp: _fx.Genesis.Timestamp,
+                transactions: _fx.Genesis.Transactions
             );
             Assert.Throws<InvalidBlockDifficultyException>(() => { invalidGenesis.Validate(); });
 
             var invalidNext = new Block<Action>(
-                index: next.Index,
+                index: _fx.Next.Index,
                 difficulty: 0, // invalid
-                nonce: next.Nonce,
-                rewardBeneficiary: next.RewardBeneficiary,
-                previousHash: next.PreviousHash,
-                timestamp: next.Timestamp,
-                transactions: next.Transactions
+                nonce: _fx.Next.Nonce,
+                rewardBeneficiary: _fx.Next.RewardBeneficiary,
+                previousHash: _fx.Next.PreviousHash,
+                timestamp: _fx.Next.Timestamp,
+                transactions: _fx.Next.Transactions
             );
             Assert.Throws<InvalidBlockDifficultyException>(() => { invalidNext.Validate(); });
         }
@@ -132,29 +127,26 @@ namespace Libplanet.Tests.Blocks
         [Fact]
         public void CanDetectInvalidPreviousHash()
         {
-            Block<Action> genesis = MineGenesis();
-            Block<Action> next = MineNext(genesis);
-
             var invalidGenesis = new Block<Action>(
-                index: genesis.Index,
-                difficulty: genesis.Difficulty,
-                nonce: genesis.Nonce,
-                rewardBeneficiary: genesis.RewardBeneficiary,
+                index: _fx.Genesis.Index,
+                difficulty: _fx.Genesis.Difficulty,
+                nonce: _fx.Genesis.Nonce,
+                rewardBeneficiary: _fx.Genesis.RewardBeneficiary,
                 previousHash: new HashDigest<SHA256>(TestUtils.GetRandomBytes(32)), // invalid
-                timestamp: genesis.Timestamp,
-                transactions: genesis.Transactions
+                timestamp: _fx.Genesis.Timestamp,
+                transactions: _fx.Genesis.Transactions
             );
 
             Assert.Throws<InvalidBlockPreviousHashException>(() => { invalidGenesis.Validate(); });
 
             var invalidNext = new Block<Action>(
-                index: next.Index,
-                difficulty: next.Difficulty,
-                nonce: next.Nonce,
-                rewardBeneficiary: next.RewardBeneficiary,
+                index: _fx.Next.Index,
+                difficulty: _fx.Next.Difficulty,
+                nonce: _fx.Next.Nonce,
+                rewardBeneficiary: _fx.Next.RewardBeneficiary,
                 previousHash: null,
-                timestamp: next.Timestamp,
-                transactions: next.Transactions
+                timestamp: _fx.Next.Timestamp,
+                transactions: _fx.Next.Transactions
             );
 
             Assert.Throws<InvalidBlockPreviousHashException>(() => { invalidNext.Validate(); });
@@ -163,9 +155,7 @@ namespace Libplanet.Tests.Blocks
         [Fact]
         public void CanConvertToRaw()
         {
-            Block<Action> genesis = MineGenesis();
-
-            RawBlock rawGenesis = genesis.ToRawBlock(true, true);
+            RawBlock rawGenesis = _fx.Genesis.ToRawBlock(true, true);
             Assert.Equal(0, rawGenesis.Index);
             Assert.Equal(0, rawGenesis.Difficulty);
             Assert.Null(rawGenesis.PreviousHash);
@@ -183,8 +173,7 @@ namespace Libplanet.Tests.Blocks
                 rawGenesis.Hash
             );
 
-            Block<Action> next = MineNext(genesis);
-            RawBlock rawNext = next.ToRawBlock(false, false);
+            RawBlock rawNext = _fx.Next.ToRawBlock(false, false);
 
             Assert.Equal(1, rawNext.Index);
             Assert.Equal(1, rawNext.Difficulty);
@@ -199,7 +188,7 @@ namespace Libplanet.Tests.Blocks
         [Fact]
         public void CanCompareToOtherBlock()
         {
-            Block<Action> sameBlock1 = MineGenesis();
+            Block<Action> sameBlock1 = _fx.Genesis;
             var sameBlock2 = new Block<Action>(
                 index: sameBlock1.Index,
                 difficulty: sameBlock1.Difficulty,
@@ -209,7 +198,7 @@ namespace Libplanet.Tests.Blocks
                 timestamp: sameBlock1.Timestamp,
                 transactions: sameBlock1.Transactions
             );
-            Block<Action> differentBlock = MineNext(sameBlock1);
+            Block<Action> differentBlock = _fx.Next;
 
             Assert.Equal(sameBlock1, sameBlock2);
             Assert.NotEqual(sameBlock2, differentBlock);
@@ -219,34 +208,6 @@ namespace Libplanet.Tests.Blocks
 
             Assert.False(sameBlock1 != sameBlock2);
             Assert.True(sameBlock2 != differentBlock);
-        }
-
-        private Block<Action> MineGenesis()
-        {
-            var rewardBenificiary = new Address(
-                ByteUtil.ParseHex("21744f4f08db23e044178dafb8273aeb5ebe6644")
-            );
-            var timestamp = new DateTime(2018, 11, 29);
-            return Block<Action>.Mine(
-                index: 0,
-                difficulty: 0,
-                rewardBeneficiary: rewardBenificiary,
-                previousHash: null,
-                timestamp: timestamp,
-                transactions: new List<Transaction<Action>>()
-            );
-        }
-
-        private Block<Action> MineNext(Block<Action> previousBlock)
-        {
-            return Block<Action>.Mine(
-                index: 1,
-                difficulty: 1,
-                rewardBeneficiary: previousBlock.RewardBeneficiary.Value,
-                previousHash: previousBlock.Hash,
-                timestamp: previousBlock.Timestamp.AddDays(1),
-                transactions: new List<Transaction<Action>>()
-            );
         }
     }
 
