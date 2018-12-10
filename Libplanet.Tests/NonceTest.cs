ca0a60ed3de9070de7b3ec26607a8f6a99bfd123
Hong Minhee <hong.minhee@gmail.com>
2018-12-10 22:02:15 +0900

    Missing unit tests for Nonce type

---
+++ Libplanet.Tests/NonceTest.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+
+namespace Libplanet.Tests
+{
+    public class NonceTest
+    {
+        [Fact]
+        public void ConstructorDoesNotTakeNullValue()
+        {
+            Assert.Throws<NullReferenceException>(() => new Nonce(null));
+        }
+
+        [Fact]
+        public void ToByteArray()
+        {
+            byte[] nonceBytes = TestUtils.GetRandomBytes(5);
+            var nonce = new Nonce(nonceBytes);
+            Assert.Equal(nonceBytes, nonce.ToByteArray());
+        }
+
+        [Fact]
+        public void ToByteArrayShouldNotExposeContents()
+        {
+            var nonce = new Nonce(new byte[5]);
+            nonce.ToByteArray()[0] = 0x01;
+
+            Assert.Equal(0x00, nonce.ToByteArray()[0]);
+        }
+
+        [Fact]
+        public void ToString_()
+        {
+            var nonce = new Nonce(
+                new byte[] { 0x00, 0x01, 0x80, 0xff }
+            );
+            Assert.Equal("000180ff", nonce.ToString());
+        }
+    }
+}
