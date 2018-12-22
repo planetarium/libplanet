1b893f97b9cd714c24976b05d720be3032bec70c
Hardywood <hardywood.kang@gmail.com>
2018-12-22 18:34:20 +0900

    List blocks

---
+++ Libplanet.Explorer/Controllers/ExplorerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using Libplanet.Action;
+using Libplanet.Blocks;
+using Libplanet.Explorer.ViewModels;
+using Libplanet.Store;
+using Libplanet.Tx;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libplanet.Explorer.Controllers
+{
+    [GenericControllerNameConvention]
+    public class ExplorerController<T> : Controller where T : IAction
+    {
+        public string TimestampFormat = "yyyy-MM-ddTHH:mm:ss.ffffffZ";
+
+        public Blockchain<T> GetBlockchain()
+        {
+            var chain = new Blockchain<T>(new FileStore("./data"));
+
+            return chain;
+        }
+
+        [HttpGet("/blocks/")]
+        public List<Dictionary<string, string>> Index()
+        {
+            Blockchain<T> chain = GetBlockchain();
+
+            return chain.Select(block => new Dictionary<string, string>
+                {
+                    { "hash", block.Hash.ToString() },
+                    { "timestamp", block.Timestamp.ToString(TimestampFormat) }
+                })
+                .ToList();
+        }
+    }
+}
