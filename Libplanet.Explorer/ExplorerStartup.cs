fff5ee8f2415dab0cd82ae158dd54963a3064abf
Hardywood <hardywood.kang@gmail.com>
2018-12-22 18:32:52 +0900

    Add Generic Startup

---
+++ Libplanet.Explorer/ExplorerStartup.cs
@@ -0,0 +1,39 @@
+﻿using Libplanet.Action;
+using Libplanet.Explorer.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Libplanet.Explorer
+{
+    public class ExplorerStartup<T>
+        where T : IAction
+    {
+        public ExplorerStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvc()
+                .ConfigureApplicationPartManager(p =>
+                    p.FeatureProviders.Add(
+                        new GenericControllerFeatureProvider<T>()));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
