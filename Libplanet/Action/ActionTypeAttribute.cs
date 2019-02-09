20d4da3e826cf41f37494509620d4841a256d1df
Hong Minhee <hong.minhee@gmail.com>
2019-02-10 06:30:18 +0900

    ActionTypeAttribute docs

--- Libplanet/Action/ActionTypeAttribute.cs
+++ Libplanet/Action/ActionTypeAttribute.cs
@@ -4,21 +4,46 @@ using System.Reflection;
 
 namespace Libplanet.Action
 {
+    /// <summary>
+    /// Indicates that an action class (i.e., a class implementing
+    /// <see cref="IAction"/>) can be held by transactions and blocks.
+    /// It also gives an action class a <see cref="TypeIdentifier"/> for
+    /// serialization and deserialization.
+    /// </summary>
     public class ActionTypeAttribute : Attribute
     {
-        private readonly string _value;
-
-        public ActionTypeAttribute(string value)
+        /// <summary>
+        /// Creates an <see cref="ActionTypeAttribute"/> with a given
+        /// <paramref name="typeIdentifier"/>.
+        /// </summary>
+        /// <param name="typeIdentifier">An action class's unique
+        /// identifier for serialization and deserialization.</param>
+        public ActionTypeAttribute(string typeIdentifier)
         {
-            _value = value;
+            TypeIdentifier = typeIdentifier;
         }
 
-        public static string ValueOf(Type t)
+        /// <summary>
+        /// An action class's unique identifier for serialization and
+        /// deserialization.
+        /// </summary>
+        public string TypeIdentifier { get; }
+
+        /// <summary>
+        /// Gets the <see cref="TypeIdentifier"/> for a given action class.
+        /// </summary>
+        /// <param name="actionType">A <see cref="Type"/> object of an action
+        /// class to know its annotated <see cref="TypeIdentifier"/>.</param>
+        /// <returns>The <see cref="TypeIdentifier"/> of the given
+        /// <paramref name="actionType"/> if it's annotated with
+        /// <see cref="ActionTypeAttribute"/>.  If it's not annotated returns
+        /// <c>null</c>.</returns>
+        public static string ValueOf(Type actionType)
         {
-            return t
+            return actionType
                 .GetCustomAttributes()
                 .OfType<ActionTypeAttribute>()
-                .Select(attr => attr._value)
+                .Select(attr => attr.TypeIdentifier)
                 .FirstOrDefault();
         }
     }
