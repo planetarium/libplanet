9a2078afc5ef086ac9346df0e720665099db2749
Hong Minhee <hong.minhee@gmail.com>
2019-02-17 00:36:11 +0900

    InvalidTxIdException docs

--- Libplanet/Tx/InvalidTxIdException.cs
+++ Libplanet/Tx/InvalidTxIdException.cs
@@ -2,9 +2,21 @@ using System;
 
 namespace Libplanet.Tx
 {
+    /// <summary>
+    /// The exception that is thrown when a given <see cref="TxId"/> cannot be
+    /// found.
+    /// </summary>
+    /// <remarks>This does <em>not</em> mean a given value is an invalid
+    /// encoding of <see cref="TxId"/>, but there is no corresponding entry to
+    /// a given <see cref="TxId"/>, which is <em>valid</em>.</remarks>
     [Serializable]
     public class InvalidTxIdException : InvalidTxException
     {
+        /// <summary>
+        /// Creates a new <see cref="InvalidTxIdException"/> object.
+        /// </summary>
+        /// <param name="message">Specifies an <see cref="Exception.Message"/>.
+        /// </param>
         public InvalidTxIdException(string message)
             : base(message)
         {
