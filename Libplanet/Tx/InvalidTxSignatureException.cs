392b915be74af0cf80f19fc44c1c9365d502af19
Hong Minhee <hong.minhee@gmail.com>
2019-02-17 00:42:50 +0900

    InvalidTxSignatureException docs

--- Libplanet/Tx/InvalidTxSignatureException.cs
+++ Libplanet/Tx/InvalidTxSignatureException.cs
@@ -2,9 +2,18 @@ using System;
 
 namespace Libplanet.Tx
 {
+    /// <summary>
+    /// The exception that is thrown when a <see cref="Transaction{T}"/>'s
+    /// <see cref="Transaction{T}.Signature"/> is invalid.
+    /// </summary>
     [Serializable]
     public class InvalidTxSignatureException : InvalidTxException
     {
+        /// <summary>
+        /// Creates a new <see cref="InvalidTxSignatureException"/> object.
+        /// </summary>
+        /// <param name="message">Specifies an <see cref="Exception.Message"/>.
+        /// </param>
         public InvalidTxSignatureException(string message)
             : base(message)
         {
