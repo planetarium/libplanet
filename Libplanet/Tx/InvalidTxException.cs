6d9755256c45d34e20dd9dbd2b8c3c09efd64efa
Hong Minhee <hong.minhee@gmail.com>
2019-02-17 00:12:55 +0900

    InvalidTxException docs

--- Libplanet/Tx/InvalidTxException.cs
+++ Libplanet/Tx/InvalidTxException.cs
@@ -2,9 +2,14 @@ using System;
 
 namespace Libplanet.Tx
 {
+    /// <summary>
+    /// Serves as the base class for exceptions relevent to
+    /// <see cref="Transaction{T}"/>.
+    /// </summary>
     public abstract class InvalidTxException : Exception
     {
         protected InvalidTxException(string message)
+            : base(message)
         {
         }
     }
