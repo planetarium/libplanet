76a2c5e35c244989427592a37c46f0b29283c8a6
Swen Mun <longfinfunnel@gmail.com>
2019-03-06 01:10:29 +0900

    Implement CRC32

---
+++ Libplanet.Tests/Net/Stun/Crc32Test.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+using Libplanet.Net.Stun;
+using Xunit;
+
+namespace Libplanet.Tests.Net.Stun
+{
+    public class Crc32Test
+    {
+        [Fact]
+        public void Calculate()
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(
+                "The quick brown fox jumps over the lazy dog");
+            uint expected = BitConverter.ToUInt32(
+                new byte[]
+                {
+                    0x39, 0xa3, 0x4f, 0x41,
+                }, 0);
+            Assert.Equal(expected, Crc32.Calculate(bytes));
+        }
+    }
+}
