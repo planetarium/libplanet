c1f17fde1631a329512f34c3bbc31b77df92fc82
Qria <qria@qria.net>
2019-03-09 16:25:32 +0900

    Make TxId serializable

--- Libplanet.Tests/Tx/TxIdTest.cs
+++ Libplanet.Tests/Tx/TxIdTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using Libplanet.Tx;
 using Xunit;
 
@@ -111,6 +113,31 @@ namespace Libplanet.Tests.Tx
 
             Assert.False(sameTxId1 != sameTxId2);
             Assert.True(sameTxId2 != differentTxId);
+        }
+
+        [Fact]
+        public void CanSerializeAndDeserialize()
+        {
+            // Serialize and deserialize to and from memory
+            var expectedTxId = new TxId(
+                new byte[]
+                {
+                    0x45, 0xa2, 0x21, 0x87, 0xe2, 0xd8, 0x85, 0x0b, 0xb3, 0x57,
+                    0x88, 0x69, 0x58, 0xbc, 0x3e, 0x85, 0x60, 0x92, 0x9c, 0xcc,
+                    0x88, 0x69, 0x58, 0xbc, 0x3e, 0x85, 0x60, 0x92, 0x9c, 0xcc,
+                    0x9c, 0xcc,
+                }
+            );
+            TxId deserializedTxId;
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var memoryStream = new MemoryStream())
+            {
+                formatter.Serialize(memoryStream, expectedTxId);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                deserializedTxId = (TxId)formatter.Deserialize(memoryStream);
+            }
+
+            Assert.Equal(deserializedTxId, expectedTxId);
         }
     }
 }
