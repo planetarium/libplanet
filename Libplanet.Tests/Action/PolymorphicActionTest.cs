abb9acb21ef91590556caa6fe42ac604c1464d1f
Hong Minhee <hong.minhee@gmail.com>
2019-04-03 16:07:48 +0900

    Missing dedicated tests for PolymorphicAction<T>

--- Libplanet.Tests/Action/PolymorphicActionTest.cs
+++ Libplanet.Tests/Action/PolymorphicActionTest.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using Libplanet.Action;
+using Libplanet.Crypto;
+using Libplanet.Tests.Common.Action;
 using Libplanet.Tx;
 using Xunit;
 
@@ -8,6 +10,66 @@ namespace Libplanet.Tests.Action
 {
     public class PolymorphicActionTest
     {
+        [Fact]
+        public void PlainValue()
+        {
+            var addr = new PrivateKey().PublicKey.ToAddress();
+            var pa = new PolymorphicAction<BaseAction>(
+                new Attack
+                {
+                    Weapon = "frying pan",
+                    Target = "mosquito",
+                    TargetAddress = addr,
+                }
+            );
+            Assert.Equal(
+                new Dictionary<string, object>
+                {
+                    { "type_id", "attack" },
+                    {
+                        "values",
+                        new Dictionary<string, object>
+                        {
+                            { "weapon", "frying pan" },
+                            { "target", "mosquito" },
+                            { "target_address", addr.ToByteArray() },
+                        }.ToImmutableDictionary()
+                    },
+                }.ToImmutableDictionary(),
+                pa.PlainValue
+            );
+        }
+
+        [Fact]
+        public void LoadPlainValue()
+        {
+            var addr = new PrivateKey().PublicKey.ToAddress();
+#pragma warning disable 612
+            var pa = new PolymorphicAction<BaseAction>();
+#pragma warning restore 612
+            pa.LoadPlainValue(
+                new Dictionary<string, object>
+                {
+                    { "type_id", "attack" },
+                    {
+                        "values",
+                        new Dictionary<string, object>
+                        {
+                            { "weapon", "frying pan" },
+                            { "target", "mosquito" },
+                            { "target_address", addr.ToByteArray() },
+                        }.ToImmutableDictionary()
+                    },
+                }.ToImmutableDictionary()
+            );
+
+            Assert.IsType<Attack>(pa.InnerAction);
+            Attack a = (Attack)pa.InnerAction;
+            Assert.Equal("frying pan", a.Weapon);
+            Assert.Equal("mosquito", a.Target);
+            Assert.Equal(addr, a.TargetAddress);
+        }
+
         [Fact]
         public void DetectLackOfActionType()
         {
