d6ca981aca60065f10682dc44afeb751b4deba0d
Seunghun Lee <waydi1@gmail.com>
2019-04-04 18:06:39 +0900

    Rename Block<T>.RewardBeneficiary to Block<T>.Miner

--- Libplanet.Explorer/ViewModels/BlockViewModel.cs
+++ Libplanet.Explorer/ViewModels/BlockViewModel.cs
@@ -9,7 +9,7 @@ namespace Libplanet.Explorer.ViewModels
         public long Index { get; set; }
         public int Difficulty { get; set; }
         public string Nonce { get; set; }
-        public string RewardBeneficiary { get; set; }
+        public string Miner { get; set; }
         public string PreviousHash { get; set; }
         public string Timestamp { get; set; }
         public List<Dictionary<string, string>> TxIds { get; set; }
