77aaf73f6d81afd6413cf433b4aeea5879872871
Hong Minhee <hong.minhee@gmail.com>
2019-04-12 22:32:49 +0900

    Debugging utility function for BitArray

--- Libplanet.Tests/TestUtils.cs
+++ Libplanet.Tests/TestUtils.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -131,6 +132,13 @@ Actual:   new byte[{actual.LongLength}] {{ {actualRepr} }}";
                 previousHash: previousHash,
                 timestamp: timestamp,
                 transactions: txs
+            );
+        }
+
+        internal static string ToString(BitArray bitArray)
+        {
+            return new string(
+                bitArray.OfType<bool>().Select(b => b ? '1' : '0').ToArray()
             );
         }
     }
