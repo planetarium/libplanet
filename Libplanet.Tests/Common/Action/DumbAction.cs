79291c2036631cdb017fa5d26470089c802ab91e
Hong Minhee <hong.minhee@gmail.com>
2019-04-18 18:30:52 +0900

    DumbAction.RecordRandom option to test a generated random value

--- Libplanet.Tests/Common/Action/DumbAction.cs
+++ Libplanet.Tests/Common/Action/DumbAction.cs
@@ -5,8 +5,11 @@ using Libplanet.Action;
 
 namespace Libplanet.Tests.Common.Action
 {
-    public sealed class DumbAction : IAction
+    public sealed class DumbAction : IAction, IEquatable<DumbAction>
     {
+        public static readonly Address RandomRecordsAddress =
+            new Address("7811C3fAa0f9Cc41F7971c3d9b031B1095b20AB2");
+
         public DumbAction()
         {
         }
@@ -14,12 +17,14 @@ namespace Libplanet.Tests.Common.Action
         public DumbAction(
             Address targetAddress,
             string item,
-            bool recordRehearsal = false
+            bool recordRehearsal = false,
+            bool recordRandom = false
         )
         {
             TargetAddress = targetAddress;
             Item = item;
             RecordRehearsal = recordRehearsal;
+            RecordRandom = recordRandom;
         }
 
         public static AsyncLocal<ImmutableList<(Address, string)>>
@@ -32,11 +37,26 @@ namespace Libplanet.Tests.Common.Action
 
         public bool RecordRehearsal { get; private set; }
 
-        public IImmutableDictionary<string, object> PlainValue =>
-            ImmutableDictionary<string, object>.Empty
-                .Add("item", Item)
-                .Add("target_address", TargetAddress.ToByteArray())
-                .Add("record_rehearsal", RecordRehearsal);
+        public bool RecordRandom { get; private set; }
+
+        public IImmutableDictionary<string, object> PlainValue
+        {
+            get
+            {
+                var plainValue = ImmutableDictionary<string, object>.Empty
+                    .Add("item", Item)
+                    .Add("target_address", TargetAddress.ToByteArray())
+                    .Add("record_rehearsal", RecordRehearsal);
+                if (RecordRandom)
+                {
+                    // In order to avoid changing tx signatures in many test
+                    // fixtures, adds field only if RecordRandom = true.
+                    plainValue = plainValue.Add("record_random", true);
+                }
+
+                return plainValue;
+            }
+        }
 
         public IAccountStateDelta Execute(IActionContext context)
         {
@@ -62,7 +82,28 @@ namespace Libplanet.Tests.Common.Action
                 ? $"{Item}:{context.Rehearsal}"
                 : Item;
             items = items is null ? item : $"{items},{item}";
+
+            if (RecordRandom)
+            {
+                states = states.SetState(
+                    RandomRecordsAddress,
+                    context.Random.Next()
+                );
+            }
+
             return states.SetState(TargetAddress, items);
+        }
+
+        public void Render(
+            IActionContext context,
+            IAccountStateDelta nextStates)
+        {
+        }
+
+        public void Unrender(
+            IActionContext context,
+            IAccountStateDelta nextStates)
+        {
         }
 
         public void LoadPlainValue(
@@ -72,6 +113,41 @@ namespace Libplanet.Tests.Common.Action
             Item = (string)plainValue["item"];
             TargetAddress = new Address((byte[])plainValue["target_address"]);
             RecordRehearsal = (bool)plainValue["record_rehearsal"];
+            RecordRandom =
+                plainValue.ContainsKey("record_random") &&
+                plainValue["record_random"] is bool r &&
+                r;
+        }
+
+        public bool Equals(DumbAction other)
+        {
+            return !ReferenceEquals(null, other) && (
+                ReferenceEquals(this, other) || (
+                    TargetAddress.Equals(other.TargetAddress) &&
+                    string.Equals(Item, other.Item) &&
+                    RecordRehearsal == other.RecordRehearsal
+                )
+            );
+        }
+
+        public override bool Equals(object obj)
+        {
+            return !ReferenceEquals(null, obj) && (
+                ReferenceEquals(this, obj) ||
+                (obj is DumbAction other && Equals(other))
+            );
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = TargetAddress.GetHashCode();
+                hashCode = (hashCode * 397) ^
+                    (Item != null ? Item.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ RecordRehearsal.GetHashCode();
+                return hashCode;
+            }
         }
     }
 }
