f6ca96cc043cbe71f0767cf2d7e9627cf7cb3e6a
Hong Minhee <hong.minhee@gmail.com>
2019-04-28 04:51:07 +0900

    Reload if code is changed

--- run.ps1
+++ run.ps1
@@ -1,2 +1,11 @@
 #!/usr/local/bin/pwsh
-dotnet run -p Libplanet.Explorer.Executable -- @args
+if ($args.Length -lt 1) {
+  $stderr = [Console]::Error.WriteLine
+  $stderr.Invoke("error: Too few arguments.")
+  $stderr.Invoke("Try ``run.ps1 --help' for more information.")
+  exit 1
+} elseif ($args.Contains("--help") -or $args.Contains("-h")) {
+  dotnet run -p Libplanet.Explorer.Executable -- @args
+  exit $?
+}
+dotnet watch -p Libplanet.Explorer.Executable -- run -- @args
