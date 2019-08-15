714e24191a3a71ee28903a7ab46c9d44444d550c
Yang Chun Ung <qooraven@gmail.com>
2019-08-15 13:58:21 +0900

    Implement deterministic Guid

--- Libplanet/Action/ActionContext.cs
+++ Libplanet/Action/ActionContext.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Libplanet.Action
 {
     internal class ActionContext : IActionContext
@@ -30,5 +32,13 @@ namespace Libplanet.Action
         public IAccountStateDelta PreviousStates { get; }
 
         public IRandom Random { get; }
+
+        public Guid NewGuid()
+        {
+            // FIXME implement rfc4122 https://www.ietf.org/rfc/rfc4122.txt
+            var b = new byte[16];
+            Random.NextBytes(b);
+            return new Guid(b);
+        }
     }
 }
