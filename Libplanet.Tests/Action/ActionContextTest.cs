a46eb1d34b28103535b2b3f683d738ec5f89c6e6
Seunghun Lee <waydi1@gmail.com>
2019-09-16 12:28:42 +0900

    Implement RFC4122

--- Libplanet.Tests/Action/ActionContextTest.cs
+++ Libplanet.Tests/Action/ActionContextTest.cs
@@ -61,12 +61,12 @@ namespace Libplanet.Tests.Action
             (Guid expected, Guid diff)[] testCases =
             {
                 (
-                    new Guid("6f460c1a-755d-d8e4-ad67-65d5f519dbc8"),
-                    new Guid("8286d046-9740-a3e4-95cf-ff46699c73c4")
+                    new Guid("6f460c1a-755d-48e4-ad67-65d5f519dbc8"),
+                    new Guid("8286d046-9740-43e4-95cf-ff46699c73c4")
                 ),
                 (
-                    new Guid("3b347c2b-f837-0085-ec5e-64005393b30d"),
-                    new Guid("3410cda1-5b13-a34e-6f84-a54adf7a0ea0")
+                    new Guid("3b347c2b-f837-4085-ac5e-64005393b30d"),
+                    new Guid("3410cda1-5b13-434e-af84-a54adf7a0ea0")
                 ),
             };
 
@@ -75,6 +75,27 @@ namespace Libplanet.Tests.Action
                 Assert.Equal(expected, context1.Random.GenerateRandomGuid());
                 Assert.Equal(expected, context2.Random.GenerateRandomGuid());
                 Assert.Equal(diff, context3.Random.GenerateRandomGuid());
+            }
+        }
+
+        [Fact]
+        public void GuidVersionAndVariant()
+        {
+            var address = new Address("21744f4f08db23e044178dafb8273aeb5ebe6644");
+
+            for (var i = 0; i < 100; i++)
+            {
+                var context = new ActionContext(
+                        signer: address,
+                        miner: address,
+                        blockIndex: 1,
+                        previousStates: new DumbAccountStateDelta(),
+                        randomSeed: i
+                    );
+                var guid = context.Random.GenerateRandomGuid().ToString();
+
+                Assert.Equal('4', guid[14]);
+                Assert.True(guid[19] >= '8' && guid[19] <= 'b');
             }
         }
 
--- Libplanet/Action/RandomExtension.cs
+++ Libplanet/Action/RandomExtension.cs
@@ -9,7 +9,8 @@ namespace Libplanet.Action
     public static class RandomExtension
     {
         /// <summary>
-        /// Generates a random <see cref="Guid"/>.
+        /// Generates a UUID version 4, i.e., a random <see cref="Guid"/>.
+        /// Please refer to <a href="https://tools.ietf.org/html/rfc4122#section-4.4">RFC 4122</a>.
         /// </summary>
         /// <param name="random"> <see cref="IRandom"/> to generate
         /// a random <see cref="Guid"/>.</param>
@@ -18,10 +19,21 @@ namespace Libplanet.Action
         /// <seealso cref="IRandom"/>
         public static Guid GenerateRandomGuid(this IRandom random)
         {
-            // FIXME implement rfc4122 https://www.ietf.org/rfc/rfc4122.txt
-            var bytes = new byte[16];
-            random.NextBytes(bytes);
-            return new Guid(bytes);
+            var b = new byte[16];
+            random.NextBytes(b);
+
+            // Set the four most significant bits (bits 12 through 15) of the time_hi_and_version
+            // field to the 4-bit version number. V4 is 0 1 0 0.
+            // This modifies b[7] not b[6] because time_hi_and_version is stored as short type
+            // in .NET like below:
+            // (short) ((int) b[7] << 8 | (int) b[6])
+            b[7] = (byte)((b[7] & 0x0f) | 0x40);
+
+            // Set the two most significant bits (bits 7 and 6) of the clock_seq_hi_and_reserved
+            // to one and zero, respectively.
+            b[8] = (byte)((b[8] & 0x3f) | 0x80);
+
+            return new Guid(b);
         }
     }
 }
