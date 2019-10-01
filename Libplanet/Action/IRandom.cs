174e46cc400ee8e817c9f608aca04e476b88e863
Pierre BOUILLON <pierre.bouillon@docapost-applicam.fr>
2019-10-01 11:15:29 +0200

    Rename variable according to #555

--- Libplanet/Action/IRandom.cs
+++ Libplanet/Action/IRandom.cs
@@ -20,42 +20,42 @@ namespace Libplanet.Action
 
         /// <summary>
         /// Gets a non-negative random integer that is less than the specified
-        /// <paramref name="maxValue"/>.
+        /// <paramref name="upperBound"/>.
         /// </summary>
-        /// <param name="maxValue">The exclusive upper bound of the random
+        /// <param name="upperBound">The exclusive upper bound of the random
         /// number to be generated.  It must be greater than or equal to 0.
         /// </param>
         /// <returns>A 32-bit signed integer that is greater than or equal to
-        /// 0 and less than <paramref name="maxValue"/>; that is, the range of
+        /// 0 and less than <paramref name="upperBound"/>; that is, the range of
         /// return values ordinarily includes 0 but not <paramref
-        /// name="maxValue"/>.  However, if <paramref name="maxValue"/> equals
-        /// to 0, <paramref name="maxValue"/> is returned.</returns>
+        /// name="upperBound"/>.  However, if <paramref name="upperBound"/> equals
+        /// to 0, <paramref name="upperBound"/> is returned.</returns>
         /// <exception cref="System.ArgumentOutOfRangeException">Thrown when
-        /// <paramref name="maxValue"/> is less than 0.</exception>
+        /// <paramref name="upperBound"/> is less than 0.</exception>
         /// <seealso cref="System.Random.Next(int)"/>
-        int Next(int maxValue);
+        int Next(int upperBound);
 
         /// <summary>
         /// Gets a random integer that is within a specified range.
         /// </summary>
-        /// <param name="minValue">The inclusive lower bound of the random
+        /// <param name="lowerBound">The inclusive lower bound of the random
         /// number to be generated.</param>
-        /// <param name="maxValue">The exclusive upper bound of the random
+        /// <param name="upperBound">The exclusive upper bound of the random
         /// number to be generated.  It must be greater than or equal to
-        /// <paramref name="minValue"/>.
+        /// <paramref name="lowerBound"/>.
         /// </param>
         /// <returns>A 32-bit signed integer that is greater than or equal to
-        /// <paramref name="minValue"/> and less than <paramref
-        /// name="maxValue"/>; that is, the range of return values ordinarily
-        /// includes <paramref name="minValue"/> but not <paramref
-        /// name="maxValue"/>.  If <paramref name="maxValue"/> equals to
-        /// <paramref name="minValue"/>, <paramref name="minValue"/> is
+        /// <paramref name="lowerBound"/> and less than <paramref
+        /// name="upperBound"/>; that is, the range of return values ordinarily
+        /// includes <paramref name="lowerBound"/> but not <paramref
+        /// name="upperBound"/>.  If <paramref name="upperBound"/> equals to
+        /// <paramref name="lowerBound"/>, <paramref name="lowerBound"/> is
         /// returned.</returns>
         /// <exception cref="ArgumentOutOfRangeException">Thrown when
-        /// <paramref name="maxValue"/> is less than <paramref
-        /// name="minValue"/>.</exception>
+        /// <paramref name="upperBound"/> is less than <paramref
+        /// name="lowerBound"/>.</exception>
         /// <seealso cref="System.Random.Next(int, int)"/>
-        int Next(int minValue, int maxValue);
+        int Next(int lowerBound, int upperBound);
 
         /// <summary>
         /// Fills the elements of a specified <see cref="byte"/>s <paramref
