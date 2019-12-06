52e82a37611be59a9a645efa2c6ab74bc62524ef
Swen Mun <longfinfunnel@gmail.com>
2019-12-06 13:08:03 +0900

    Limit retry count on MessageRequest

--- Libplanet/Net/Swarm.cs
+++ Libplanet/Net/Swarm.cs
@@ -2250,15 +2250,22 @@ namespace Libplanet.Net
                 }
                 catch (Exception e)
                 {
-                    const int retryAfter = 100;
-                    _logger.Debug(
-                        $"Unexpected exception occurred during {nameof(ProcessRuntime)}(): " +
-                        "{Exception}; retry after {DelayMs} ms...",
-                        e,
-                        retryAfter
-                    );
-                    await _requests.AddAsync(req, cancellationToken);
-                    await Task.Delay(retryAfter, cancellationToken);
+                    if (req.Retryable)
+                    {
+                        const int retryAfter = 100;
+                        _logger.Debug(
+                            $"Unexpected exception occurred during {nameof(ProcessRuntime)}(): " +
+                            "{Exception}; retry after {DelayMs} ms...",
+                            e,
+                            retryAfter
+                        );
+                        await _requests.AddAsync(req.Retry(), cancellationToken);
+                        await Task.Delay(retryAfter, cancellationToken);
+                    }
+                    else
+                    {
+                        _logger.Error("Failed to process request[{req}]; discard it.", req);
+                    }
                 }
             }
         }
@@ -2434,6 +2441,8 @@ namespace Libplanet.Net
 
         private readonly struct MessageRequest
         {
+            private readonly int _retried;
+
             public MessageRequest(
                 in Guid id,
                 Message message,
@@ -2441,6 +2450,26 @@ namespace Libplanet.Net
                 in TimeSpan? timeout,
                 in int expectedResponses,
                 TaskCompletionSource<IEnumerable<Message>> taskCompletionSource)
+                : this(
+                      id,
+                      message,
+                      peer,
+                      timeout,
+                      expectedResponses,
+                      taskCompletionSource,
+                      0
+                    )
+            {
+            }
+
+            internal MessageRequest(
+                in Guid id,
+                Message message,
+                BoundPeer peer,
+                in TimeSpan? timeout,
+                in int expectedResponses,
+                TaskCompletionSource<IEnumerable<Message>> taskCompletionSource,
+                int retried)
             {
                 Id = id;
                 Message = message;
@@ -2448,6 +2477,7 @@ namespace Libplanet.Net
                 Timeout = timeout;
                 ExpectedResponses = expectedResponses;
                 TaskCompletionSource = taskCompletionSource;
+                _retried = retried;
             }
 
             public Guid Id { get; }
@@ -2461,6 +2491,21 @@ namespace Libplanet.Net
             public int ExpectedResponses { get; }
 
             public TaskCompletionSource<IEnumerable<Message>> TaskCompletionSource { get; }
+
+            public bool Retryable => _retried < 10;
+
+            public MessageRequest Retry()
+            {
+                return new MessageRequest(
+                    Id,
+                    Message,
+                    Peer,
+                    Timeout,
+                    ExpectedResponses,
+                    TaskCompletionSource,
+                    _retried + 1
+                );
+            }
         }
     }
 }
