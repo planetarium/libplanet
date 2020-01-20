9fd4e814736b9aef684bbfed1fd4f1968d2d2059
GitHub <noreply@github.com>
2020-01-20 12:38:42 +0000

    Publish docs from a3a05af5b57152e3fcf75d34924d64b8514b1f80

--- master/styles/main.js
+++ master/styles/main.js
@@ -1,1 +1,9 @@
-// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+(function () {
+  // Disable DocFX's default footer behavior.
+  var resetFooter = function () {
+    $('footer').attr('style', '');
+  };
+  $.fn.fadeIn = resetFooter;
+  $.fn.fadeOut = resetFooter;
+  resetFooter();
+})();
