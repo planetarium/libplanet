7450060601877c816f1ccbbd5effb819d65cf934
Ko Chanhyuck <lime_bell@naver.com>
2020-01-29 18:45:11 +0900

    Add document to PeerState class

--- Libplanet/Net/PeerState.cs
+++ Libplanet/Net/PeerState.cs
@@ -2,9 +2,12 @@ using System;
 
 namespace Libplanet.Net
 {
+    /// <summary>
+    /// Represents a <see cref="Peer"/>'s state in the routing table.
+    /// </summary>
     public class PeerState
     {
-        public PeerState(BoundPeer peer, DateTimeOffset lastUpdated)
+        internal PeerState(BoundPeer peer, DateTimeOffset lastUpdated)
         {
             Peer = peer;
             LastUpdated = lastUpdated;
@@ -12,12 +15,24 @@ namespace Libplanet.Net
             Latency = null;
         }
 
+        /// <summary>
+        /// <see cref="Address"/> of peer.
+        /// </summary>
         public Address Address => Peer.Address;
 
+        /// <summary>
+        /// Last time messages were exchanged.
+        /// </summary>
         public DateTimeOffset LastUpdated { get; internal set; }
 
+        /// <summary>
+        /// Last time the peer was verified.
+        /// </summary>
         public DateTimeOffset? LastChecked { get; internal set; }
 
+        /// <summary>
+        /// Delay of verification in milliseconds.
+        /// </summary>
         public TimeSpan? Latency { get; internal set; }
 
         internal BoundPeer Peer { get; set; }
