b2ba822e214a89da329d100b5e11d7fda2218b89
Seunghun Lee <waydi1@gmail.com>
2020-02-27 20:09:46 +0900

    Add StartsWith extension method for byte array

--- Libplanet.Tests/ByteUtilTest.cs
+++ Libplanet.Tests/ByteUtilTest.cs
@@ -60,5 +60,26 @@ namespace Libplanet.Tests
                 ByteUtil.CalculateHashCode(otherBytes)
             );
         }
+
+        [Fact]
+        public void StartsWith()
+        {
+            byte[] bytes = null;
+
+            Assert.Throws<ArgumentNullException>(() => bytes.StartsWith(new byte[] { 0 }));
+            bytes = new byte[]
+            {
+                0, 1,
+            };
+
+            Assert.Throws<ArgumentNullException>(() => bytes.StartsWith(null));
+
+            Assert.False(bytes.StartsWith(new byte[] { 0, 1, 2 }));
+            Assert.False(bytes.StartsWith(new byte[] { 0, 2 }));
+            Assert.False(bytes.StartsWith(new byte[] { 1 }));
+
+            Assert.True(bytes.StartsWith(new byte[] { 0 }));
+            Assert.True(bytes.StartsWith(new byte[] { 0, 1 }));
+        }
     }
 }
