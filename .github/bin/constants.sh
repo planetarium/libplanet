6e93e65e7ebfdb44a4dcbd44f54b4fba23d785f0
Hong Minhee <hong.minhee@gmail.com>
2020-03-30 16:32:41 +0900

    Build single file executable

--- .github/bin/constants.sh
+++ .github/bin/constants.sh
@@ -3,4 +3,6 @@
 # shellcheck disable=SC2034
 projects=("Libplanet" "Libplanet.RocksDBStore" "Libplanet.Tools")
 configuration=Release
-
+executables=("Libplanet.Tools")
+rids=(linux-x64 osx-x64 win-x64)
+# https://docs.microsoft.com/en-us/dotnet/core/rid-catalog
