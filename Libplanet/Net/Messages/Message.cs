1efe8be55ebd582ab6b8425f6fb2aa470e1aa6e6
Ko Chanhyuck <lime_bell@naver.com>
2020-07-21 11:24:29 +0900

    Apply suggestions

--- Libplanet/Net/Messages/Message.cs
+++ Libplanet/Net/Messages/Message.cs
@@ -21,7 +21,7 @@ namespace Libplanet.Net.Messages
             /// <summary>
             /// A reply to <see cref="Ping"/>.
             /// </summary>
-            Pong = 0x15,
+            Pong = 0x14,
 
             /// <summary>
             /// Request to query block hashes.
