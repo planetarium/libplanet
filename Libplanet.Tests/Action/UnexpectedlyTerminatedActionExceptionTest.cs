450e1e8eb165fe77ca82f692885d5bcf27bfc4f6
Swen Mun <longfinfunnel@gmail.com>
2020-07-29 22:11:00 +0900

    Fill un-covered serialization code in UnexpectedlyTerminatedActionException

--- Libplanet.Tests/Action/UnexpectedlyTerminatedActionExceptionTest.cs
+++ Libplanet.Tests/Action/UnexpectedlyTerminatedActionExceptionTest.cs
@@ -1,7 +1,10 @@
 using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
 using Libplanet.Action;
+using Libplanet.Tests.Common.Action;
+using Libplanet.Tx;
 using Xunit;
 
 namespace Libplanet.Tests.Action
@@ -12,8 +15,23 @@ namespace Libplanet.Tests.Action
         public void Serializable()
         {
             var innerExc = new Exception("inner");
+            var blockHash = new HashDigest<SHA256>(
+                TestUtils.GetRandomBytes(HashDigest<SHA256>.Size)
+            );
+            long blockIndex = 100;
+            var txId = new TxId(TestUtils.GetRandomBytes(TxId.Size));
+            var action = new Sleep()
+            {
+                ZoneId = 1,
+            };
+
             var exc = new UnexpectedlyTerminatedActionException(
-                default, default, null, null, "for testing", innerExc
+                blockHash,
+                blockIndex,
+                txId,
+                action,
+                "for testing",
+                innerExc
             );
 
             var formatter = new BinaryFormatter();
@@ -26,6 +44,13 @@ namespace Libplanet.Tests.Action
                 Assert.Equal("for testing", deserialized.Message);
                 Assert.IsType<Exception>(deserialized.InnerException);
                 Assert.Equal(innerExc.Message, deserialized.InnerException.Message);
+
+                Assert.Equal(blockHash, deserialized.BlockHash);
+                Assert.Equal(blockIndex, deserialized.BlockIndex);
+                Assert.Equal(txId, deserialized.TxId);
+
+                Assert.IsType<Sleep>(deserialized.Action);
+                Assert.Equal(action.PlainValue, deserialized.Action.PlainValue);
             }
         }
     }
