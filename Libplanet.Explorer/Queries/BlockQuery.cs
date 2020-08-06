f182dcb33c91f5311ed533c10a01412956811597
Muhun Kim <iam@muhun.kim>
2020-08-06 11:10:43 +0900

    Hash and index cannot coexist

--- Libplanet.Explorer/Queries/BlockQuery.cs
+++ Libplanet.Explorer/Queries/BlockQuery.cs
@@ -49,11 +49,16 @@ namespace Libplanet.Explorer.Queries
                 resolve: context =>
                 {
                     string? hash = context.GetArgument<string?>("hash", null);
+                    long? index = context.GetArgument<long?>("index", null);
+
+                    if ((hash is string && index is long) || (hash is null && index is null)) {
+                        throw new System.Exception("Unexpected block query");    
+                    }
+
                     if (hash is string hashNotNull) {
                         return Query<T>.GetBlockByHash(HashDigest<SHA256>.FromString(hashNotNull));
                     }
 
-                    long? index = context.GetArgument<long?>("index", null);
 
                     if (index is long indexNotNull) {
                         return Query<T>.GetBlockByIndex(indexNotNull);
