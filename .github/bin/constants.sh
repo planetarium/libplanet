e687b8d542e2c4bf9c60bd33505c52c1f01f0473
Lee Suho <riemannulus@hitagi.moe>
2020-08-10 10:35:28 +0900

    master to main

--- .github/bin/constants.sh
+++ .github/bin/constants.sh
@@ -9,11 +9,11 @@ executables=("Libplanet.Tools")
 rids=(linux-x64 osx-x64 win-x64)
 
 # Publish a package only if the repository is upstream (planetarium/libplanet)
-# and the branch is for releases (master or *-maintenance or 9c-*).
+# and the branch is for releases (main or *-maintenance or 9c-*).
 # shellcheck disable=SC2235
 if [ "$GITHUB_REPOSITORY" = "planetarium/libplanet" ] && [[ \
     "$GITHUB_REF" = refs/tags/* || \
-    "$GITHUB_REF" = refs/heads/master || \
+    "$GITHUB_REF" = refs/heads/main || \
     "$GITHUB_REF" = refs/heads/*-maintenance || \
     "$GITHUB_REF" = refs/heads/9c-* \
   ]]; then
