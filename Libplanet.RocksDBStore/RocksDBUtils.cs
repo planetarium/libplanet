8b9e5650a07c6ad1463e2b8f8baef2491fe73be6
moreal <dev.moreal@gmail.com>
2020-08-18 20:32:31 +0900

    Separate `RocksDBStore.OpenRocksDb` logic

---
+++ Libplanet.RocksDBStore/RocksDBUtils.cs
@@ -0,0 +1,15 @@
+using RocksDbSharp;
+
+namespace Libplanet.RocksDBStore
+{
+    public static class RocksDBUtils
+    {
+        internal static RocksDb OpenRocksDb(
+            DbOptions options, string dbPath, ColumnFamilies columnFamilies = null)
+        {
+            return columnFamilies is null
+                ? RocksDb.Open(options, dbPath)
+                : RocksDb.Open(options, dbPath, columnFamilies);
+        }
+    }
+}
