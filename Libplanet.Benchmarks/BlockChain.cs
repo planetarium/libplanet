1451f28c173cf2ad7c4551e863c0ab11220e3f04
moreal <dev.moreal@gmail.com>
2020-10-28 19:56:31 +0900

    feat(benchmark): add benchmark for `GetNextTxNonce()`
    [skip changelog]

--- Libplanet.Benchmarks/BlockChain.cs
+++ Libplanet.Benchmarks/BlockChain.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Libplanet.Blockchain;
@@ -14,6 +16,8 @@ namespace Libplanet.Benchmarks
     {
         private StoreFixture _fx;
         private BlockChain<DumbAction> _blockChain;
+
+        private ImmutableArray<PrivateKey> _privateKeys;
 
         public BlockChain()
         {
@@ -44,6 +48,37 @@ namespace Libplanet.Benchmarks
         public void ContainsBlock()
         {
             _blockChain.ContainsBlock(_blockChain.Tip.Hash);
+        }
+
+        [IterationSetup(Target = nameof(GetNextTxNonce))]
+        public void SetupChain_GetNextTxNonce()
+        {
+            _fx = new DefaultStoreFixture();
+            _blockChain = new BlockChain<DumbAction>(
+                new NullPolicy<DumbAction>(),
+                _fx.Store,
+                _fx.StateStore,
+                _fx.GenesisBlock);
+
+            const int privateKeyCount = 3;
+            _privateKeys = Enumerable.Range(0, privateKeyCount)
+                .Select(_ => new PrivateKey())
+                .ToImmutableArray();
+            foreach (PrivateKey privateKey in _privateKeys)
+            {
+                for (var i = 0; i < 2000; i++)
+                {
+                    _blockChain.MakeTransaction(privateKey, new DumbAction[0]);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void GetNextTxNonce()
+        {
+            _blockChain.GetNextTxNonce(_privateKeys[0].ToAddress());
+            _blockChain.GetNextTxNonce(_privateKeys[1].ToAddress());
+            _blockChain.GetNextTxNonce(_privateKeys[2].ToAddress());
         }
     }
 }
