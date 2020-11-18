9f833697ed6e33538f237a5a325a61aafb767a58
Ko Chanhyuck <lime_bell@naver.com>
2020-11-18 10:42:43 +0900

    Add FIXME comment

--- Libplanet/Net/Swarm.cs
+++ Libplanet/Net/Swarm.cs
@@ -1981,6 +1981,8 @@ namespace Libplanet.Net
                 {
                     using (await _blockSyncMutex.LockAsync(cancellationToken))
                     {
+                        // FIXME: Should only reset when BlockDemand has not changed
+                        // from the beginning of this operation.
                         _logger.Debug(
                             $"{nameof(ProcessFillBlocks)} finished. Reset {nameof(BlockDemand)}");
                         BlockDemand = null;
