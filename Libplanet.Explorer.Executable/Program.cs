6a4fb7d159bc519090bc87ccba24c6e5f4a102d9
longfin <longfinfunnel@gmail.com>
2021-02-09 20:21:53 +0900

    Bump libplanet to 0.11.0-dev.20210205090355

--- Libplanet.Explorer.Executable/Program.cs
+++ Libplanet.Explorer.Executable/Program.cs
@@ -274,6 +274,8 @@ namespace Libplanet.Explorer.Executable
 
             public int MaxTransactionsPerBlock => _impl.MaxTransactionsPerBlock;
 
+            public IComparer<IBlockExcerpt> CanonicalChainComparer => _impl.CanonicalChainComparer;
+
             public bool DoesTransactionFollowsPolicy(
                 Transaction<AppAgnosticAction> transaction, BlockChain<AppAgnosticAction> blockChain
             )
