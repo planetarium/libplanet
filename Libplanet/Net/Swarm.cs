8795c84770debdebc979c8d613896a0775f149cd
Swen Mun <longfinfunnel@gmail.com>
2021-06-29 14:03:18 +0900

    Update Libplanet/Net/Swarm.cs
    Co-authored-by: Say Cheong <greymistcube@gmail.com>

--- Libplanet/Net/Swarm.cs
+++ Libplanet/Net/Swarm.cs
@@ -310,10 +310,10 @@ namespace Libplanet.Net
         /// the dial-up is cancelled after this timeout, and it tries another peer.
         /// If <c>null</c> is given it never gives up dial-ups.
         /// </param>
-        /// <param name="broadcastBlockInterval">The time period of broadcasting chain tip.
-        /// </param>
-        /// <param name="broadcastTxInterval">The time period of exchange of staged transactions.
-        /// </param>
+        /// <param name="broadcastBlockInterval">Time interval between each broadcast of
+        /// chain tip.</param>
+        /// <param name="broadcastTxInterval">Time interval between each broadcast of staged
+        /// transactions.</param>
         /// <param name="cancellationToken">
         /// A cancellation token used to propagate notification that this
         /// operation should be canceled.
