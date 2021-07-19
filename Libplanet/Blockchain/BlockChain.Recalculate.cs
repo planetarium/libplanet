659210520462eee78bcfd3034fca8c95cfdff258
Say Cheong <greymistcube@gmail.com>
2021-07-19 15:15:57 +0900

    Tail complementation added

--- Libplanet/Blockchain/BlockChain.Recalculate.cs
+++ Libplanet/Blockchain/BlockChain.Recalculate.cs
@@ -70,7 +70,7 @@ namespace Libplanet.Blockchain
         /// </para>
         /// <para>
         /// Unlike <see cref="RecalculateBlockStates"/>, this method skips recalculations if states
-        /// are found for intermediate blocks. Note that this is not fully secure if states for
+        /// are found for intermediate blocks. This may not be fully secure if states for
         /// blocks in <see cref="IStateStore"/> are already corrupted.
         /// </para>
         /// </remarks>
@@ -94,8 +94,7 @@ namespace Libplanet.Blockchain
 
                 IReadOnlyList<ActionEvaluation> evaluations = ActionEvaluator.Evaluate(
                     block,
-                    stateCompleters
-                );
+                    stateCompleters);
 
                 _rwlock.EnterWriteLock();
                 try
@@ -114,9 +113,54 @@ namespace Libplanet.Blockchain
             }
         }
 
+        /// <summary>
+        /// Recalculates and complements all <i>missing</i> block states including and upto given
+        /// <paramref name="blockHash"/> starting from the last known states in
+        /// <see cref="StateStore"/> before <paramref name="blockHash"/> if the states are missing
+        /// for <paramref name="blockHash"/>.
+        /// </summary>
+        /// <param name="blockHash">The starting point for searching backwards.</param>
         internal void TailComplementBlockStates(BlockHash blockHash)
         {
-            throw new NotSupportedException("Placeholder.");
+            _logger.Verbose("Complementing block states upto {BlockHash}...", blockHash);
+
+            // Prevent recursive trial to recalculate & complement incomplete block states by
+            // mistake; if the below code works as intended, these state completers must never
+            // be invoked.
+            StateCompleterSet<T> stateCompleters = StateCompleterSet<T>.Reject;
+            Stack<BlockHash> stack = new Stack<BlockHash>();
+            BlockHash? pointer = blockHash;
+
+            while (pointer is { } p)
+            {
+                if (StateStore.ContainsBlockStates(p))
+                {
+                    break;
+                }
+                else
+                {
+                    stack.Push(p);
+                    pointer = this[p].PreviousHash;
+                }
+            }
+
+            foreach (BlockHash hash in stack)
+            {
+                Block<T> block = this[hash];
+                IReadOnlyList<ActionEvaluation> evaluations = ActionEvaluator.Evaluate(
+                    block,
+                    stateCompleters);
+
+                _rwlock.EnterWriteLock();
+                try
+                {
+                    SetStates(block, evaluations);
+                }
+                finally
+                {
+                    _rwlock.ExitWriteLock();
+                }
+            }
         }
     }
 }
