a35fd560310829caf397bb5b829b442ba1d8ba0b
Say Cheong <greymistcube@gmail.com>
2021-07-19 10:56:13 +0900

    Full complementation added
    Minor cleanup

--- Libplanet/Blockchain/BlockChain.Recalculate.cs
+++ Libplanet/Blockchain/BlockChain.Recalculate.cs
@@ -1,5 +1,6 @@
 using Libplanet.Action;
 using Libplanet.Blocks;
+using Libplanet.Store;
 using System.Collections.Generic;
 using System;
 
@@ -56,9 +57,61 @@ namespace Libplanet.Blockchain
             }
         }
 
+        /// <summary>
+        /// Recalculates and complements all <i>missing</i> block states including and upto given
+        /// <paramref name="blockHash"/> starting from the genesis block.
+        /// </summary>
+        /// <param name="blockHash">The inclusive limit of target hash to terminate complementation.
+        /// </param>
+        /// <remarks>
+        /// <para>
+        /// If a complementation of the entire blockchain is needed, call with the tip hash of the
+        /// <see cref="BlockChain{T}"/>.
+        /// </para>
+        /// <para>
+        /// Unlike <see cref="RecalculateBlockStates"/>, this method skips recalculations if states
+        /// are found for intermediate blocks. Note that this is not fully secure if states for
+        /// blocks in <see cref="IStateStore"/> are already corrupted.
+        /// </para>
+        /// </remarks>
         internal void FullComplementBlockStates(BlockHash blockHash)
         {
-            throw new NotImplementedException("Placeholder.");
+            _logger.Verbose("Complementing block states upto {BlockHash}...", blockHash);
+
+            // Prevent recursive trial to recalculate & complement incomplete block states by
+            // mistake; if the below code works as intended, these state completers must never
+            // be invoked.
+            StateCompleterSet<T> stateCompleters = StateCompleterSet<T>.Reject;
+
+            // Calculates and fills the incomplete states on the fly.
+            foreach (BlockHash hash in BlockHashes)
+            {
+                Block<T> block = this[hash];
+                if (StateStore.ContainsBlockStates(hash))
+                {
+                    continue;
+                }
+
+                IReadOnlyList<ActionEvaluation> evaluations = ActionEvaluator.Evaluate(
+                    block,
+                    stateCompleters
+                );
+
+                _rwlock.EnterWriteLock();
+                try
+                {
+                    SetStates(block, evaluations);
+                }
+                finally
+                {
+                    _rwlock.ExitWriteLock();
+                }
+
+                if (blockHash.Equals(hash))
+                {
+                    break;
+                }
+            }
         }
 
         internal void TailComplementBlockStates(BlockHash blockHash)
