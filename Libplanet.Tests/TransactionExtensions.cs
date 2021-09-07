47188671637e91a44891b33d80662da7a8c6a1d7
Hong Minhee <hong.minhee@gmail.com>
2021-09-07 13:55:44 +0900

    TransactionExtensions.Validate() for better debug
    It's useful for debugging outdated invalid tx signatures in test fixtures.
    
    [changelog skip]

---
+++ Libplanet.Tests/TransactionExtensions.cs
@@ -0,0 +1,47 @@
+using Libplanet.Action;
+using Libplanet.Crypto;
+using Libplanet.Tx;
+
+namespace Libplanet.Tests
+{
+    public static class TransactionExtensions
+    {
+        /// <summary>
+        /// Almost equivalent to <see cref="Transaction{T}.Validate()"/>, except that when
+        /// the transaction's signature is invalid, it throws an <see
+        /// cref="InvalidSignatureException"/> with a better error message, which suggests
+        /// the valid one as well so that someone who is debugging the test can change
+        /// the hard-coded outdated signature of fixture data.
+        /// </summary>
+        /// <param name="tx">The transaction to validate.</param>
+        /// <param name="privateKey">The private key corresponding to the signer.</param>
+        public static void Validate<T>(this Transaction<T> tx, PrivateKey privateKey)
+            where T : IAction, new()
+        {
+            try
+            {
+                tx.Validate();
+            }
+            catch (InvalidTxSignatureException)
+            {
+                if (!privateKey.ToAddress().Equals(tx.PublicKey.ToAddress()))
+                {
+                    throw new InvalidTxPublicKeyException(
+                        tx.Id,
+                        "The given private key does not correspond to the transaction's public key."
+                    );
+                }
+
+                byte[] serialized = tx.Serialize(false);
+                byte[] validSignature = privateKey.Sign(serialized);
+                throw new InvalidTxSignatureException(
+                    tx.Id,
+                    "The transaction's signature is invalid.  " +
+                    "Suggest you to change it to the valid one instead.\n" +
+                    $"Actual invalid signature:  {ByteUtil.Hex(tx.Signature)}\n" +
+                    $"Suggested valid signature: {ByteUtil.Hex(validSignature)}"
+                );
+            }
+        }
+    }
+}
