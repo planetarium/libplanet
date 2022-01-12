4e0df516049873ae667242e8dd00a3c187bca366
Say Cheong <greymistcube@gmail.com>
2022-01-12 16:57:55 +0900

    [Skip changelog] More tests
    More tests
    
    More tests

--- Libplanet.Tests/Blockchain/BlockChainTest.Stage.cs
+++ Libplanet.Tests/Blockchain/BlockChainTest.Stage.cs
@@ -45,7 +45,7 @@ namespace Libplanet.Tests.Blockchain
                 default(BlockHash),
                 new List<DumbAction>());
 
-            _blockChain.StageTransaction(tx1);
+            Assert.True(_blockChain.StageTransaction(tx1));
             Assert.Equal(1, _blockChain.GetStagedTransactionIds().Count);
             Assert.Throws<InvalidTxGenesisHashException>(() => _blockChain.StageTransaction(tx2));
             Assert.Equal(1, _blockChain.GetStagedTransactionIds().Count);
@@ -54,30 +54,62 @@ namespace Libplanet.Tests.Blockchain
         }
 
         [Fact]
-        public async Task TransactionsWithDuplicatedNonceShouldBeFiltered()
+        public async Task TransactionsWithDuplicatedNonce()
         {
             var key = new PrivateKey();
-            Transaction<DumbAction> txWithValidNonce = _fx.MakeTransaction(
+
+            Transaction<DumbAction> tx_0_0 = _fx.MakeTransaction(
                 new DumbAction[0],
                 ImmutableHashSet<Address>.Empty,
+                nonce: 0,
                 privateKey: key
             );
-            _blockChain.StageTransaction(txWithValidNonce);
+            Transaction<DumbAction> tx_0_1 = _fx.MakeTransaction(
+                new DumbAction[0],
+                ImmutableHashSet<Address>.Empty,
+                nonce: 0,
+                privateKey: key
+            );
+            Transaction<DumbAction> tx_1_0 = _fx.MakeTransaction(
+                new DumbAction[0],
+                ImmutableHashSet<Address>.Empty,
+                nonce: 1,
+                privateKey: key
+            );
+            Transaction<DumbAction> tx_1_1 = _fx.MakeTransaction(
+                new DumbAction[0],
+                ImmutableHashSet<Address>.Empty,
+                nonce: 1,
+                privateKey: key
+            );
+
+            // stage tx_0_0 -> mine tx_0_0 -> stage tx_0_1
+            Assert.True(_blockChain.StageTransaction(tx_0_0));
             await _blockChain.MineBlock(key);
             Assert.Empty(_blockChain.GetStagedTransactionIds());
             Assert.Empty(_blockChain.StagePolicy.Iterate(_blockChain, filtered: true));
             Assert.Empty(_blockChain.StagePolicy.Iterate(_blockChain, filtered: false));
-
-            Transaction<DumbAction> txWithInvalidNonce = _fx.MakeTransaction(
-                new DumbAction[0],
-                ImmutableHashSet<Address>.Empty,
-                nonce: txWithValidNonce.Nonce,
-                privateKey: key
-            );
-            _blockChain.StageTransaction(txWithInvalidNonce);
+            // should still able to stage a low nonce tx
+            Assert.True(_blockChain.StageTransaction(tx_0_1));
+            // tx_0_1 is still staged, just filtered.
             Assert.Empty(_blockChain.GetStagedTransactionIds());
             Assert.Empty(_blockChain.StagePolicy.Iterate(_blockChain, filtered: true));
             Assert.NotEmpty(_blockChain.StagePolicy.Iterate(_blockChain, filtered: false));
+
+            // stage tx_1_0 -> stage tx_1_1 -> mine tx_1_0 or tx_1_1
+            Assert.True(_blockChain.StageTransaction(tx_1_0));
+            Assert.True(_blockChain.StageTransaction(tx_1_1));
+            var txIds = new List<TxId>() { tx_1_0.Id, tx_1_1.Id };
+            Assert.Equal(2, _blockChain.GetStagedTransactionIds().Count);
+            Assert.Equal(
+                txIds.OrderBy(id => id),
+                _blockChain.GetStagedTransactionIds().OrderBy(id => id)
+            );
+            await _blockChain.MineBlock(key);
+            // tx_0_1 and tx_1_x should be still staged, just filtered
+            Assert.Empty(_blockChain.GetStagedTransactionIds());
+            Assert.Empty(_blockChain.StagePolicy.Iterate(_blockChain, filtered: true));
+            Assert.Equal(2, _blockChain.StagePolicy.Iterate(_blockChain, filtered: false).Count());
         }
 
         [Fact]
@@ -93,8 +125,8 @@ namespace Libplanet.Tests.Blockchain
 
             Assert.Equal(txIds, stagedTxIds);
 
-            _blockChain.UnstageTransaction(_fx.Transaction1);
-            _blockChain.UnstageTransaction(_fx.Transaction2);
+            Assert.True(_blockChain.UnstageTransaction(_fx.Transaction1));
+            Assert.True(_blockChain.UnstageTransaction(_fx.Transaction2));
 
             Assert.Empty(_blockChain.GetStagedTransactionIds());
         }
