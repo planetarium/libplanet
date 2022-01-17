5f9d058697e979660fd872a182ebbe3c9e7713b7
Lee <riemannulus@hitagi.moe>
2022-01-17 20:34:33 +0900

    docs: XML comment for `BlockCandidateTable<T>`

--- Libplanet/Net/BlockCandidateTable.cs
+++ Libplanet/Net/BlockCandidateTable.cs
@@ -4,13 +4,20 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using Libplanet.Action;
+using Libplanet.Blockchain;
 using Libplanet.Blocks;
 using Serilog;
 
 namespace Libplanet.Net
 {
+    /// <summary>
+    /// A class which table data structure that stores <see cref="Block{T}"/>s
+    /// received from <see cref="Peer"/>.
+    /// </summary>
+    /// <typeparam name="T">An <see cref="IAction"/> type.  It should match
+    /// to <see cref="Block{T}"/>'s type parameter.</typeparam>
     public class BlockCandidateTable<T>
-    where T : IAction, new()
+        where T : IAction, new()
     {
         private readonly ConcurrentDictionary<BlockHeader, SortedList<long, Block<T>>> _blocks;
 
@@ -26,6 +33,12 @@ namespace Libplanet.Net
 
         public bool Any() => !_blocks.IsEmpty;
 
+        /// <summary>
+        /// Adds a <see cref="Block{T}"/>s to the table.
+        /// </summary>
+        /// <param name="blockHeader">This is the header of the <see cref="BlockChain{T}"/>
+        /// tip at the time of downloading the blocks.</param>
+        /// <param name="blocks">List of downloaded <see cref="Block{T}"/>.</param>
         public void Add(BlockHeader blockHeader, IEnumerable<Block<T>> blocks)
         {
             if (_blocks.ContainsKey(blockHeader))
@@ -51,6 +64,12 @@ namespace Libplanet.Net
             }
         }
 
+        /// <summary>
+        /// Get the <see cref="Block{T}"/>s which are in the table.
+        /// </summary>
+        /// <param name="thisRoundTip">Canonical <see cref="BlockChain{T}"/>'s
+        /// tip of this round.</param>
+        /// <returns><see cref="Block{T}"/>s by <paramref name="thisRoundTip"/>.</returns>
         public SortedList<long, Block<T>>? GetCurrentRoundCandidate(
             BlockHeader thisRoundTip)
         {
