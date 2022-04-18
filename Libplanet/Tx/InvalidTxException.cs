8a0c116f7ed3a3d37a5b00d76546ede352b60378
Colibri Shin <colibrishin@gmail.com>
2022-04-18 19:35:46 +0900

    Add Inner-exception param in base TxExceptions

--- Libplanet/Tx/InvalidTxException.cs
+++ Libplanet/Tx/InvalidTxException.cs
@@ -15,13 +15,15 @@ namespace Libplanet.Tx
         /// <see cref="Transaction{T}.Id"/>.  It is automatically included to
         /// the <see cref="Exception.Message"/> string.</param>
         /// <param name="message">A descriptive error message for programmers.
+        /// </param> <param name="inner">A Inner-exception.
         /// </param>
-        protected InvalidTxException(TxId txid, string message)
-            : base($"{txid.ToHex()}: {message}")
+#nullable enable
+        protected InvalidTxException(TxId txid, string message, Exception? inner)
+            : base($"{txid.ToHex()}: {message}", inner)
         {
             TxId = txid;
         }
-
+#nullable disable
         /// <summary>
         /// The <see cref="TxId"/> of the invalid <see cref="Transaction{T}"/>.
         /// </summary>
