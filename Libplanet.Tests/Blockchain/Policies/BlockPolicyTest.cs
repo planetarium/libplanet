fd161421570d57cd62b107bcdba501f05a033676
Colibri Shin <colibrishin@gmail.com>
2022-04-19 13:18:53 +0900

    test code for TxPolicy that has inner-exception

--- Libplanet.Tests/Blockchain/Policies/BlockPolicyTest.cs
+++ Libplanet.Tests/Blockchain/Policies/BlockPolicyTest.cs
@@ -154,6 +154,31 @@ namespace Libplanet.Tests.Blockchain.Policies
             var invalidTx = _chain.MakeTransaction(invalidKey, new DumbAction[] { });
             expected = policy.ValidateNextBlockTx(_chain, invalidTx);
             Assert.NotNull(expected);
+            Assert.Null(expected.InnerException);
+        }
+
+        [Fact]
+        public void ValidateNextBlockTxNoInnerException()
+        {
+            var validKey = new PrivateKey();
+
+            TxPolicyViolationException IsSignerValid(
+                BlockChain<DumbAction> chain, Transaction<DumbAction> tx)
+            {
+                var validAddress = validKey.PublicKey.ToAddress();
+                return tx.Signer.Equals(validAddress)
+                    ? null
+                    : new TxPolicyViolationException(tx.Id, "invalid signer");
+            }
+
+            var policy = new BlockPolicy<DumbAction>(validateNextBlockTx: IsSignerValid);
+
+            // Invalid Transaction
+            var invalidKey = new PrivateKey();
+            var invalidTx = _chain.MakeTransaction(invalidKey, new DumbAction[] { });
+            var expected = policy.ValidateNextBlockTx(_chain, invalidTx);
+            Assert.NotNull(expected);
+            Assert.Null(expected.InnerException);
         }
 
         [Fact]
