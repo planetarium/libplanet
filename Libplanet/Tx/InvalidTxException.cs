3a493d300b26b72082326dc30c135cb28bc5a98e
Colibri Shin <colibrishin@gmail.com>
2022-04-25 11:17:59 +0900

    ctor. with InnerException in `InvalidTxException` and `TxPolicyViolationException`

--- Libplanet/Tx/InvalidTxException.cs
+++ Libplanet/Tx/InvalidTxException.cs
@@ -9,7 +9,9 @@ namespace Libplanet.Tx
     public abstract class InvalidTxException : Exception
     {
         /// <summary>
-        /// Instantiates a new exception object with proper metadata.
+        /// Instantiates a new exception object with proper metadata. Use
+        /// <see cref="InvalidTxException(TxId, string, Exception)"/> if innerException
+        /// is required.
         /// </summary>
         /// <param name="txid">The invalid <see cref="Transaction{T}"/>'s
         /// <see cref="Transaction{T}.Id"/>.  It is automatically included to
@@ -18,6 +20,23 @@ namespace Libplanet.Tx
         /// </param>
         protected InvalidTxException(TxId txid, string message)
             : base($"{txid.ToHex()}: {message}")
+        {
+            TxId = txid;
+        }
+
+        /// <inheritdoc cref="InvalidTxException(TxId, string)"/>
+        /// <param name="txid">The invalid <see cref="Transaction{T}"/>'s
+        /// <see cref="Transaction{T}.Id"/>.  It is automatically included to
+        /// the <see cref="Exception.Message"/> string.</param>
+        /// <param name="message">A descriptive error message for programmers.
+        /// </param>
+        /// <param name="innerException">The <see cref="Exception"/> for
+        /// <see cref="Exception.InnerException"/>.</param>
+        protected InvalidTxException(
+            TxId txid,
+            string message,
+            Exception innerException)
+            : base($"{txid.ToHex()}: {message}", innerException)
         {
             TxId = txid;
         }
