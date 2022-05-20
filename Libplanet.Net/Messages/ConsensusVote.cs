89402edf7cb3636574435468ce4ce1494eab954c
Lee, Suho <iam@suho.io>
2022-05-20 19:13:29 +0900

    refactor: use `Vote` struct on the `ConsensusMessage`

--- Libplanet.Net/Messages/ConsensusVote.cs
+++ Libplanet.Net/Messages/ConsensusVote.cs
@@ -1,17 +1,33 @@
-using Libplanet.Blocks;
+using System.Collections.Generic;
+using Libplanet.Consensus;
 
 namespace Libplanet.Net.Messages
 {
     public class ConsensusVote : ConsensusMessage
     {
-        public ConsensusVote(long nodeId, long height, long round, BlockHash blockHash)
-            : base(nodeId, height, round, blockHash)
+        public ConsensusVote(Vote vote)
+            : base(vote.NodeId, vote.Height, vote.Round, vote.BlockHash)
         {
+            ProposeVote = vote;
         }
 
         public ConsensusVote(byte[][] dataframes)
-            : base(dataframes)
+            : this(new Vote(dataframes[0]))
         {
+        }
+
+        public Vote ProposeVote { get; }
+
+        public override IEnumerable<byte[]> DataFrames
+        {
+            get
+            {
+                var frames = new List<byte[]>
+                {
+                    ProposeVote.ByteArray,
+                };
+                return frames;
+            }
         }
 
         public override MessageType Type => MessageType.ConsensusVote;
