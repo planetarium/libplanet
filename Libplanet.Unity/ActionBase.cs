79bfd7596ec29b62b13c6a76101bea39a5e84a33
Say Cheong <greymistcube@gmail.com>
2022-06-09 17:42:34 +0900

    [skip changelog] ActionBase added

---
+++ Libplanet.Unity/ActionBase.cs
@@ -0,0 +1,21 @@
+using Bencodex.Types;
+using Libplanet.Action;
+
+namespace LibplanetUnity.Action
+{
+    /// <summary>
+    /// A base class for <see cref="IAction"/>s.  Inherit this class when implementing
+    /// concrete game actions.
+    /// </summary>
+    public abstract class ActionBase : IAction
+    {
+        /// <inheritdoc/>
+        public abstract IValue PlainValue { get; }
+
+        /// <inheritdoc/>
+        public abstract void LoadPlainValue(IValue plainValue);
+
+        /// <inheritdoc/>
+        public abstract IAccountStateDelta Execute(IActionContext context);
+    }
+}
