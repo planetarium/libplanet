460f9167e589dfdfcf236a8848efe9ea1bbcce70
Say Cheong <greymistcube@gmail.com>
2022-06-10 16:42:04 +0900

    [skip changelog] Paths and Utils for Unity

---
+++ Libplanet.Unity/Paths.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using Libplanet.Blocks;
+using Libplanet.Crypto;
+using Libplanet.Node;
+using UnityEngine;
+
+namespace Libplanet.Unity
+{
+    /// <summary>
+    /// Various file paths for storing persistent data.
+    /// </summary>
+    public static class Paths
+    {
+        /// <summary>
+        /// Path to storing a genesis <see cref="Block{T}"/>.
+        /// </summary>
+        public static readonly string GenesisBlockPath =
+            Path.Combine(Application.streamingAssetsPath, "genesis");
+
+        /// <summary>
+        /// Path to storing a <see cref="SwarmConfig"/>.
+        /// </summary>
+        public static readonly string SwarmConfigPath =
+            Path.Combine(Application.streamingAssetsPath, "swarm_config.json");
+
+        /// <summary>
+        /// Path to storing chain data.
+        /// </summary>
+        public static readonly string StorePath =
+            Path.Combine(Application.persistentDataPath, "store");
+
+        /// <summary>
+        /// Path to storing a <see cref="PrivateKey"/>.
+        /// </summary>
+        public static readonly string PrivateKeyPath =
+            Path.Combine(Application.persistentDataPath, "private_key");
+    }
+}
---
+++ Libplanet.Unity/Utils.cs
@@ -0,0 +1,52 @@
+using System.IO;
+using Libplanet.Action;
+using Libplanet.Blocks;
+using Libplanet.Crypto;
+using Libplanet.Node;
+using Libplanet.Unity;
+
+namespace LibplanetUnity
+{
+    /// <summary>
+    /// Various helper methods for creating persistent data.
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Creates a new <see cref="SwarmConfig"/> and writes to <paramref name="path"/>.
+        /// If one already exists at <paramref name="path"/>, overwrites it.
+        /// </summary>
+        /// <param name="path">File path to save a <see cref="SwarmConfig"/>.</param>
+        public static void CreateSwarmConfig(string path)
+        {
+            SwarmConfig swarmConfig = new SwarmConfig();
+            File.Delete(path);
+            File.WriteAllText(path, swarmConfig.ToJson());
+        }
+
+        /// <summary>
+        /// Creates a new genesis <see cref="Block{T}"/> and writes to <paramref name="path"/>.
+        /// If one already exists at <paramref name="path"/>, overwrites it.
+        /// </summary>
+        /// <param name="path">The path to save a genesis <see cref="Block{T}"/>.</param>
+        public static void CreateGenesisBlock(string path)
+        {
+            Block<PolymorphicAction<ActionBase>> genesisBlock =
+                NodeUtils<PolymorphicAction<ActionBase>>.CreateGenesisBlock();
+            File.Delete(path);
+            NodeUtils<PolymorphicAction<ActionBase>>.SaveGenesisBlock(path, genesisBlock);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="PrivateKey"/> and writes to <paramref name="path"/>.
+        /// If one already exists at <paramref name="path"/>, overwrites it.
+        /// </summary>
+        /// <param name="path">The path to save a <see cref="PrivateKey"/>.</param>
+        public static void CreatePrivateKey(string path)
+        {
+            PrivateKey privateKey = new PrivateKey();
+            File.Delete(path);
+            NodeUtils<PolymorphicAction<ActionBase>>.SavePrivateKey(path, privateKey);
+        }
+    }
+}
