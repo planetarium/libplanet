e4158c16b289e4af9f7f3e3f662b68f3fefc8928
Colibri Shin <colibrishin@gmail.com>
2022-06-30 17:55:48 +0900

    chore/cleanup: comment exceptions

--- Libplanet.Net/Consensus/InvalidBlockProposeMessageException.cs
+++ Libplanet.Net/Consensus/InvalidBlockProposeMessageException.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Runtime.Serialization;
+using Libplanet.Blocks;
 using Libplanet.Net.Messages;
 
 namespace Libplanet.Net.Consensus
 {
+    /// <summary>
+    /// An exception thrown when a <see cref="ConsensusPropose"/> has invalid
+    /// <see cref="BlockHash"/> (i.e., NIL).
+    /// </summary>
     [Serializable]
     public class InvalidBlockProposeMessageException : InvalidMessageException
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="InvalidBlockProposeMessageException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.
+        /// </param>
+        /// <param name="receivedMessage">A message that has invalid <see cref="BlockHash"/>.
+        /// </param>
+        /// <param name="innerException">An <see cref="Exception"/> that occurs this exception.
+        /// </param>
         public InvalidBlockProposeMessageException(
             string message,
             Message receivedMessage,
@@ -15,11 +29,28 @@ namespace Libplanet.Net.Consensus
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="InvalidBlockProposeMessageException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.
+        /// </param>
+        /// <param name="receivedMessage">A message that has invalid <see cref="BlockHash"/>.
+        /// </param>
         public InvalidBlockProposeMessageException(string message, Message receivedMessage)
             : base(message, receivedMessage)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidBlockProposeMessageException"/>
+        /// class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo" />
+        /// that holds the serialized object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">The <see cref="StreamingContext" />
+        /// that contains contextual information about the source or destination.
+        /// </param>
         protected InvalidBlockProposeMessageException(
             SerializationInfo info,
             StreamingContext context)
--- Libplanet.Net/Consensus/InvalidHeightIncreasingException.cs
+++ Libplanet.Net/Consensus/InvalidHeightIncreasingException.cs
@@ -1,12 +1,40 @@
 using System;
+using System.Runtime.Serialization;
+using Libplanet.Blockchain;
 
 namespace Libplanet.Net.Consensus
 {
+    /// <summary>
+    /// An exception thrown when a <see cref="ConsensusContext{T}.NewHeight"/> has called with
+    /// height is not the index of <see cref="BlockChain{T}.Tip"/> + 1.
+    /// </summary>
     [Serializable]
     public class InvalidHeightIncreasingException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="InvalidHeightIncreasingException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.
+        /// </param>
         public InvalidHeightIncreasingException(string message)
             : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidHeightIncreasingException"/>
+        /// class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo" />
+        /// that holds the serialized object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">The <see cref="StreamingContext" />
+        /// that contains contextual information about the source or destination.
+        /// </param>
+        protected InvalidHeightIncreasingException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
         {
         }
     }
