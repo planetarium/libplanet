e06127b52dedac0beb190d12ee86aaa98eea4f2c
Colibri Shin <colibrishin@gmail.com>
2022-06-30 17:56:00 +0900

    chore: comment `ConsensusReactor`

--- Libplanet.Net/Consensus/ConsensusReactor.cs
+++ Libplanet.Net/Consensus/ConsensusReactor.cs
@@ -14,6 +14,12 @@ using Serilog;
 
 namespace Libplanet.Net.Consensus
 {
+    /// <summary>
+    /// A manager class for starting network and joining into consensus.
+    /// <seealso cref="ConsensusContext{T}"/>
+    /// </summary>
+    /// <typeparam name="T">An <see cref="IAction"/> type of <see cref="BlockChain{T}"/>.
+    /// </typeparam>
     public class ConsensusReactor<T> : IReactor
         where T : IAction, new()
     {
@@ -23,6 +29,23 @@ namespace Libplanet.Net.Consensus
         private ConsensusContext<T> _consensusContext;
         private BlockChain<T> _blockChain;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsensusReactor{T}"/> class.
+        /// </summary>
+        /// <param name="consensusTransport">An <see cref="ITransport"/> for sending the
+        /// <see cref="ConsensusMessage"/>s to validators.</param>
+        /// <param name="blockChain">A blockchain that will be committed, which
+        /// will be voted by consensus, and used for proposing a block.
+        /// </param>
+        /// <param name="privateKey">A <see cref="PrivateKey"/> for using in signing a block,
+        /// message.
+        /// </param>
+        /// <param name="validatorPeers">A list of validator's <see cref="PublicKey"/>, including
+        /// itself.
+        /// </param>
+        /// <param name="newHeightDelay">A time delay in starting the consensus for the next height
+        /// block. <seealso cref="ConsensusContext{T}.OnBlockChainTipChanged"/>
+        /// </param>
         public ConsensusReactor(
             ITransport consensusTransport,
             BlockChain<T> blockChain,
@@ -50,16 +73,31 @@ namespace Libplanet.Net.Consensus
                 .ForContext("Source", nameof(ConsensusReactor<T>));
         }
 
+        /// <summary>
+        /// A delegate method for <see cref="ConsensusReactor{T}.CheckValidatorsLiveness"/>.
+        /// </summary>
         private delegate Task<bool> PingPong(BoundPeer peer);
 
+        /// <summary>
+        /// Whether this <see cref="ConsensusReactor{T}"/> is running.
+        /// </summary>
         public bool Running => _consensusTransport.Running;
 
+        /// <summary>
+        /// <inheritdoc cref="IDisposable.Dispose()"/>
+        /// </summary>
         public void Dispose()
         {
             _consensusTransport.Dispose();
             _consensusContext.Dispose();
         }
 
+        /// <summary>
+        /// Starts the instance and joins into consensus.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token used to propagate notification
+        /// that this operation should be canceled.</param>
+        /// <returns>Returns the <see cref="ITransport.StartAsync"/>.</returns>
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             Task task = _consensusTransport.StartAsync(cancellationToken);
@@ -69,12 +107,22 @@ namespace Libplanet.Net.Consensus
             await task;
         }
 
+        /// <summary>
+        /// Stops the instance and consensus.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation Token.</param>
+        /// <returns>Returns the <see cref="ITransport.StopAsync"/>.</returns>
         public async Task StopAsync(CancellationToken cancellationToken)
         {
             _consensusContext.Dispose();
             await _consensusTransport.StopAsync(TimeSpan.FromMilliseconds(10), cancellationToken);
         }
 
+        /// <summary>
+        /// Returns a summary of current consensus status in JSON-formatted string.
+        /// </summary>
+        /// <returns>Returns a summary in JSON-formatted string.
+        /// </returns>
         public override string ToString()
         {
             var dict =
@@ -85,11 +133,18 @@ namespace Libplanet.Net.Consensus
             return JsonSerializer.Serialize(dict);
         }
 
-        internal void BroadcastMessage(ConsensusMessage message)
-        {
+        /// <summary>
+        /// Broadcasts <see cref="ConsensusMessage"/> to validators.
+        /// </summary>
+        /// <param name="message">A <see cref="ConsensusMessage"/> to broadcast.</param>
+        private void BroadcastMessage(ConsensusMessage message) =>
             _consensusTransport.BroadcastMessage(_validatorPeers, message);
-        }
 
+        /// <summary>
+        /// A handler for <see cref="ITransport.ProcessMessageHandler"/> to handles a received
+        /// <see cref="Message"/>s.
+        /// </summary>
+        /// <param name="message">A message to process.</param>
         private async Task ProcessMessageHandler(Message message)
         {
             switch (message)
@@ -104,6 +159,10 @@ namespace Libplanet.Net.Consensus
             }
         }
 
+        /// <summary>
+        /// A task for checking how many validators are alive.
+        /// </summary>
+        /// <param name="ctx">A cancellation token.</param>
         private async Task CheckValidatorsLiveness(CancellationToken ctx)
         {
             while (!ctx.IsCancellationRequested)
@@ -146,6 +205,10 @@ namespace Libplanet.Net.Consensus
             }
         }
 
+        /// <summary>
+        /// Replies a <see cref="Pong"/> of received <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">A message to replies.</param>
         private async Task ReplyMessagePongAsync(Message message)
         {
             var pong = new Pong { Identity = message.Identity };
