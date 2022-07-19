fd6c42c48c9794033e2c08bcf881836128d452be
Chanhyuck Ko <limeelbee@gmail.com>
2022-07-19 16:40:29 +0900

    test: add test for Message.GetHash()

--- Libplanet.Net.Tests/Messages/MessageTest.cs
+++ Libplanet.Net.Tests/Messages/MessageTest.cs
@@ -14,14 +14,8 @@ using static Libplanet.Tests.TestUtils;
 
 namespace Libplanet.Net.Tests.Messages
 {
-    [Collection("NetMQConfiguration")]
-    public class MessageTest : IDisposable
+    public class MessageTest
     {
-        public void Dispose()
-        {
-            NetMQConfig.Cleanup(false);
-        }
-
         [Fact]
         public void BlockHeaderMessage()
         {
@@ -107,6 +101,38 @@ namespace Libplanet.Net.Tests.Messages
                 default(Address));
             Assert.Throws<ArgumentException>(
                 () => codec.Decode(new NetMQMessage(), true));
+        }
+
+        [Fact]
+        public void GetHash()
+        {
+            var privateKey = new PrivateKey();
+            var peer = new Peer(privateKey.PublicKey);
+            var apv = new AppProtocolVersion(
+                1,
+                new Bencodex.Types.Integer(0),
+                ImmutableArray<byte>.Empty,
+                default(Address));
+            var dateTimeOffset = DateTimeOffset.MinValue + TimeSpan.FromHours(6.1234);
+            Block<DumbAction> genesis = MineGenesisBlock<DumbAction>(
+                _ => HashAlgorithmType.Of<SHA256>(),
+                GenesisMiner
+            );
+            var message = new BlockHeaderMessage(genesis.Hash, genesis.Header)
+            {
+                Version = apv,
+                Timestamp = dateTimeOffset,
+                Remote = peer,
+            };
+            AssertBytesEqual(
+                new byte[32]
+                {
+                    0x49, 0xf1, 0x9d, 0x6b, 0x21, 0x4e, 0x46, 0x43,
+                    0x73, 0x39, 0x84, 0xec, 0x19, 0xf6, 0x55, 0xd5,
+                    0x34, 0x7e, 0xbe, 0xf3, 0x4a, 0x8c, 0x5b, 0xdd,
+                    0x2f, 0x46, 0x80, 0xe7, 0xbb, 0xdd, 0xa0, 0x0c,
+                },
+                message.GetHash());
         }
     }
 }
