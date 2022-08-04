8e29bbd5578b63b5c011a26b45e88f7115831fa9
Swen Mun <longfinfunnel@gmail.com>
2022-08-04 12:23:12 +0900

    Apply review

--- Libplanet.Explorer/GraphTypes/BencodexValueType.cs
+++ Libplanet.Explorer/GraphTypes/BencodexValueType.cs
@@ -11,7 +11,7 @@ namespace Libplanet.Explorer.GraphTypes
 
         public BencodexValueType()
         {
-            Name = "IValue";
+            Name = "BencodexValue";
         }
 
         public override object? Serialize(object? value)
