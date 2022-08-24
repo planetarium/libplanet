bcaf0e28fcc4689c64607097e9aab14919da31c4
ilgyu <ilgyu@planetariumhq.com>
2022-08-24 21:47:08 +0900

    fix: fix error on big integer encoding / decoding

--- Libplanet.Tests/Crypto/VrfTest.cs
+++ Libplanet.Tests/Crypto/VrfTest.cs
@@ -1,19 +1,11 @@
 using System;
 using Libplanet.Crypto;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Libplanet.Tests.Crypto
 {
     public class VrfTest
     {
-        private readonly ITestOutputHelper _output;
-
-        public VrfTest(ITestOutputHelper output)
-        {
-            _output = output;
-        }
-
         [Fact]
         public void ChecksumEqualTest()
         {
@@ -26,6 +18,22 @@ namespace Libplanet.Tests.Crypto
             (byte[] index1, byte[] proof) = VerifiableRandomFunction.Evaluate(message, privateKey);
             byte[] index2 = VerifiableRandomFunction.ProofToHash(message, proof, publicKey);
             Assert.Equal(index1, index2);
+        }
+
+        [Fact]
+        public void ChecksumInequalTest()
+        {
+            PrivateKey privateKeyA = new PrivateKey();
+            PublicKey publicKeyA = privateKeyA.PublicKey;
+            PrivateKey privateKeyB = new PrivateKey();
+            PublicKey publicKeyB = privateKeyB.PublicKey;
+            Random random = new Random();
+            byte[] message = new byte[1024];
+            random.NextBytes(message);
+
+            (byte[] index1, byte[] proof) = VerifiableRandomFunction.Evaluate(message, privateKeyA);
+            Assert.Throws<ArgumentException>(
+                () => VerifiableRandomFunction.ProofToHash(message, proof, publicKeyB));
         }
     }
 }
