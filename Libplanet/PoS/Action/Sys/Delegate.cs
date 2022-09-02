5ce21ffcce323575974d2fc2321138a60c478eed
Lee, Suho <iam@suho.io>
2022-09-03 03:21:26 +0900

    refactor: Delegate

---
+++ Libplanet/PoS/Action/Sys/Delegate.cs
@@ -0,0 +1,63 @@
+using Bencodex.Types;
+using Libplanet.Assets;
+using Libplanet.PoS;
+
+namespace Libplanet.Action.Sys
+{
+    /// <summary>
+    /// A system action for dPoS that <see cref="Delegate"/> specified <see cref="Amount"/>
+    /// of tokens to a given <see cref="Validator"/>.
+    /// </summary>
+    public sealed class Delegate : IAction
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="Delegate"/> action.
+        /// </summary>
+        /// <param name="validator">The <see cref="Address"/> of the validator
+        /// to delegate tokens.</param>
+        /// <param name="amount">The amount of the asset to be delegated.</param>
+        public Delegate(Address validator, FungibleAssetValue amount)
+        {
+            Validator = validator;
+            Amount = amount;
+        }
+
+        internal Delegate()
+        {
+            // Used only for deserialization.  See also class Libplanet.Action.Sys.Registry.
+        }
+
+        /// <summary>
+        /// The <see cref="Address"/> of the validator to <see cref="Delegation"/>.
+        /// </summary>
+        public Address Validator { get; set; }
+
+        public FungibleAssetValue Amount { get; set; }
+
+        /// <inheritdoc cref="IAction.PlainValue"/>
+        public IValue PlainValue => Bencodex.Types.Dictionary.Empty
+            .Add("validator", Validator.Serialize())
+            .Add("amount", Amount.Serialize());
+
+        /// <inheritdoc cref="IAction.LoadPlainValue(IValue)"/>
+        public void LoadPlainValue(IValue plainValue)
+        {
+            var dict = (Bencodex.Types.Dictionary)plainValue;
+            Validator = dict["validator"].ToAddress();
+            Amount = dict["amount"].ToFungibleAssetValue();
+        }
+
+        /// <inheritdoc cref="IAction.Execute(IActionContext)"/>
+        public IAccountStateDelta Execute(IActionContext context)
+        {
+            IActionContext ctx = context;
+            var states = ctx.PreviousStates;
+
+            // if (ctx.Rehearsal)
+            // Rehearsal mode is not implemented
+            states = DelegateCtrl.Execute(states, ctx.Signer, Validator, Amount);
+
+            return states;
+        }
+    }
+}
