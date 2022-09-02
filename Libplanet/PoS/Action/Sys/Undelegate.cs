0ae4a39f3261e8910ddbcfa68cebf1db8bdd0af9
Lee, Suho <iam@suho.io>
2022-09-03 03:22:10 +0900

    refactor: Undelegate

---
+++ Libplanet/PoS/Action/Sys/Undelegate.cs
@@ -0,0 +1,67 @@
+using Bencodex.Types;
+using Libplanet.Assets;
+using Libplanet.PoS;
+
+namespace Libplanet.Action.Sys
+{
+    /// <summary>
+    /// A system action for dPoS that cancel <see cref="Delegate"/> specified
+    /// <see cref="ShareAmount"/> of shared tokens to a given <see cref="Validator"/>.
+    /// </summary>
+    public sealed class Undelegate : IAction
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="CancelUndelegation"/> action.
+        /// </summary>
+        /// <param name="validator">The <see cref="Address"/> of the validator
+        /// to undelegate tokens.</param>
+        /// <param name="amount">The amount of the asset to be undelegated.</param>
+        public Undelegate(Address validator, FungibleAssetValue amount)
+        {
+            Validator = validator;
+            ShareAmount = amount;
+        }
+
+        internal Undelegate()
+        {
+            // Used only for deserialization.  See also class Libplanet.Action.Sys.Registry.
+        }
+
+        /// <summary>
+        /// The <see cref="Address"/> of the validator to cancel the <see cref="Delegate"/>.
+        /// </summary>
+        public Address Validator { get; set; }
+
+        /// <summary>
+        /// The amount of the asset to be undelegate.
+        /// </summary>
+        public FungibleAssetValue ShareAmount { get; set; }
+
+        /// <inheritdoc cref="IAction.PlainValue"/>
+        public IValue PlainValue => Bencodex.Types.Dictionary.Empty
+            .Add("validator", Validator.Serialize())
+            .Add("amount", ShareAmount.Serialize());
+
+        /// <inheritdoc cref="IAction.LoadPlainValue(IValue)"/>
+        public void LoadPlainValue(IValue plainValue)
+        {
+            var dict = (Bencodex.Types.Dictionary)plainValue;
+            Validator = dict["validator"].ToAddress();
+            ShareAmount = dict["amount"].ToFungibleAssetValue();
+        }
+
+        /// <inheritdoc cref="IAction.Execute(IActionContext)"/>
+        public IAccountStateDelta Execute(IActionContext context)
+        {
+            IActionContext ctx = context;
+            var states = ctx.PreviousStates;
+
+            // if (ctx.Rehearsal)
+            // Rehearsal mode is not implemented
+            states = UndelegateCtrl.Execute(
+                states, ctx.Signer, Validator, ShareAmount, ctx.BlockIndex);
+
+            return states;
+        }
+    }
+}
