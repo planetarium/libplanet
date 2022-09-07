9831f8c723ed2262349e88f141e49ba320fd5647
Hong Minhee <hong.minhee@gmail.com>
2022-09-07 15:34:19 +0900

    @planetarium/tx: PublicKey type

--- @planetarium/tx/test/address.test.ts
+++ @planetarium/tx/test/address.test.ts
@@ -1,15 +1,7 @@
 import { test } from "vitest";
 import * as fc from "fast-check";
 import { encodeAddress } from "../src/address";
-
-function bytesEqual(
-  a: Uint8Array | ArrayBuffer,
-  b: Uint8Array | ArrayBuffer
-): boolean {
-  const x = a instanceof ArrayBuffer ? new Uint8Array(a) : a;
-  const y = b instanceof ArrayBuffer ? new Uint8Array(b) : b;
-  return x.length === y.length && x.every((v, i) => v === y[i]);
-}
+import { bytesEqual } from "./bytes";
 
 test("encodeAddress", () => {
   fc.assert(
