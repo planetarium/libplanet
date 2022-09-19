0aa907083f4063dd0e93c4e0ffc491a79e58cd8e
ilgyu <ilgyu@planetariumhq.com>
2022-09-19 23:29:18 +0900

    feat: change access modifier for reserved addresses

--- Libplanet/PoS/Misc/ReservedAddress.cs
+++ Libplanet/PoS/Misc/ReservedAddress.cs
@@ -1,35 +1,35 @@
 namespace Libplanet.PoS
 {
-    internal static class ReservedAddress
+    public static class ReservedAddress
     {
-        internal static readonly Address BondedPool
+        public static readonly Address BondedPool
             = new Address("0000000000000000000000000000000000000001");
 
-        internal static readonly Address UnbondedPool
+        public static readonly Address UnbondedPool
             = new Address("0000000000000000000000000000000000000002");
 
-        internal static readonly Address RewardPool
+        public static readonly Address RewardPool
             = new Address("0000000000000000000000000000000000000003");
 
-        internal static readonly Address ValidatorPowerIndex
+        public static readonly Address ValidatorPowerIndex
             = new Address("0000000000000000000000000000000000000004");
 
-        internal static readonly Address PreviousBondedValidatorSet
+        public static readonly Address PreviousBondedValidatorSet
             = new Address("0000000000000000000000000000000000000005");
 
-        internal static readonly Address BondedValidatorSet
+        public static readonly Address BondedValidatorSet
             = new Address("0000000000000000000000000000000000000006");
 
-        internal static readonly Address UnbondedValidatorSet
+        public static readonly Address UnbondedValidatorSet
             = new Address("0000000000000000000000000000000000000007");
 
-        internal static readonly Address UnbondingSet
+        public static readonly Address UnbondingSet
             = new Address("0000000000000000000000000000000000000008");
 
-        internal static readonly Address BlockRewardHistory
+        public static readonly Address BlockRewardHistory
             = new Address("0000000000000000000000000000000000000009");
 
-        internal static readonly Address CommunityPool
+        public static readonly Address CommunityPool
             = new Address("0000000000000000000000000000000000000010");
     }
 }
