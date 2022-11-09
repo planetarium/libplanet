321255643c7c0ded57e0eb7054c72b63dfee5e47
Hyun Seungmin <boscohyun@gmail.com>
2022-11-09 17:17:14 +0900

    add some rocksdb arguments related with readonly

--- Libplanet.RocksDBStore/RocksDBUtils.cs
+++ Libplanet.RocksDBStore/RocksDBUtils.cs
@@ -6,11 +6,22 @@ namespace Libplanet.RocksDBStore
     internal static class RocksDBUtils
     {
         internal static RocksDb OpenRocksDb(
-            DbOptions options, string dbPath, ColumnFamilies? columnFamilies = null)
+            DbOptions options,
+            string dbPath,
+            ColumnFamilies? columnFamilies = null,
+            bool readOnly = false,
+            bool errIfLogFileExists = false)
         {
             if (!Directory.Exists(dbPath))
             {
                 Directory.CreateDirectory(dbPath);
+            }
+
+            if (readOnly)
+            {
+                return columnFamilies is null
+                    ? RocksDb.OpenReadOnly(options, dbPath, errIfLogFileExists)
+                    : RocksDb.OpenReadOnly(options, dbPath, columnFamilies, errIfLogFileExists);
             }
 
             return columnFamilies is null
