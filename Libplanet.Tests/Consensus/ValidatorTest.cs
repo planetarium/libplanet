c21809f205c3cb2c8d8db39e2588da810f6506a4
ilgyu <ilgyu@planetariumhq.com>
2022-11-29 18:45:56 +0900

    test: update

---
+++ Libplanet.Tests/Consensus/ValidatorTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Numerics;
+using Libplanet.Consensus;
+using Libplanet.Crypto;
+using Xunit;
+
+namespace Libplanet.Tests.Consensus
+{
+    public class ValidatorTest
+    {
+        [Fact]
+        public void Marshalling()
+        {
+            PublicKey publicKey = new PrivateKey().PublicKey;
+            Validator validator = new Validator(publicKey, BigInteger.One);
+            Validator unmarshalledValidator = new Validator(validator.ByteArray);
+            Assert.Equal(validator, unmarshalledValidator);
+            Assert.Equal(validator.PublicKey, unmarshalledValidator.PublicKey);
+            Assert.Equal(validator.Power, unmarshalledValidator.Power);
+        }
+    }
+}
