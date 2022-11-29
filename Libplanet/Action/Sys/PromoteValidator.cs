d15c5feacc8a0916d3f3ce9f95b58fe2ea6f0412
ilgyu <ilgyu@planetariumhq.com>
2022-11-29 19:23:28 +0900

    fix: change promotion action parameter

--- Libplanet/Action/Sys/PromoteValidator.cs
+++ Libplanet/Action/Sys/PromoteValidator.cs
@@ -1,6 +1,5 @@
 using System.Numerics;
 using Bencodex.Types;
-using Libplanet.Consensus;
 using Libplanet.Crypto;
 
 namespace Libplanet.Action.Sys
@@ -18,23 +17,32 @@ namespace Libplanet.Action.Sys
         /// <param name="power">The power of the validator.</param>
         public PromoteValidator(PublicKey validatorPublicKey, BigInteger power)
         {
-            Validator = new Validator(validatorPublicKey, power);
+            ValidatorPublicKey = validatorPublicKey;
+            Power = power;
         }
 
         /// <summary>
-        /// The <see cref="PublicKey"/> of the target promoting to a validator.
+        /// The <see cref="PublicKey"/> of the operator promoting to a validator.
         /// </summary>
-        public Validator Validator { get; set; }
+        public PublicKey ValidatorPublicKey { get; set; }
+
+        /// <summary>
+        /// The <see langword="Power"/> of the target operator promoting to a validator.
+        /// </summary>
+        public BigInteger Power { get; set; }
 
         /// <inheritdoc cref="IAction.PlainValue"/>
         public IValue PlainValue => Dictionary.Empty
-            .Add("validator", Validator.ByteArray);
+            .Add("validator_public_key", ValidatorPublicKey.Format(false))
+            .Add("power", Power.ToByteArray());
 
         /// <inheritdoc cref="IAction.LoadPlainValue(IValue)"/>
         public void LoadPlainValue(IValue plainValue)
         {
             var dict = (Dictionary)plainValue;
-            Validator = new Validator(dict.GetValue<Binary>("validator"));
+            ValidatorPublicKey = new PublicKey(
+                dict.GetValue<Binary>("validator_public_key").ByteArray);
+            Power = new BigInteger(dict.GetValue<Binary>("power"));
         }
 
         /// <inheritdoc cref="IAction.Execute(IActionContext)"/>
@@ -42,7 +50,7 @@ namespace Libplanet.Action.Sys
         {
             IActionContext ctx = context;
             var states = ctx.PreviousStates;
-            states = states.PromoteValidator(Validator);
+            states = states.PromoteValidator(ValidatorPublicKey, Power);
             return states;
         }
     }
