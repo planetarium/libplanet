2ad080cc85e8426c8176e7e2c11929731aa414ef
github-action-benchmark <github@users.noreply.github.com>
2022-12-14 04:17:04 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 8761c9a0208a512406ee3b1bd32df33999dc622b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670991414513,
+  "lastUpdate": 1670991424559,
   "repoUrl": "https://github.com/planetarium/libplanet",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -30,6 +30,66 @@ window.BENCHMARK_DATA = {
             "value": 109702.61538461539,
             "unit": "ns",
             "range": "± 1478.7833703454528"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "riemannulus@hitagi.moe",
+            "name": "Lee, Suho",
+            "username": "riemannulus"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8761c9a0208a512406ee3b1bd32df33999dc622b",
+          "message": "Merge pull request #2634 from riemannulus/fix/ci/benchmark/exclusive-options\n\nRemove exclusive option",
+          "timestamp": "2022-12-14T13:06:11+09:00",
+          "tree_id": "5e888d4168401483cf3971d9048e2cd22183e0a5",
+          "url": "https://github.com/planetarium/libplanet/commit/8761c9a0208a512406ee3b1bd32df33999dc622b"
+        },
+        "date": 1670991417307,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Libplanet.Benchmarks.DataModel.DataModelBenchmark.EncodeRootModel",
+            "value": 5886771.078125,
+            "unit": "ns",
+            "range": "± 35712.947931649716"
+          },
+          {
+            "name": "Libplanet.Benchmarks.DataModel.DataModelBenchmark.EncodeLeafModel",
+            "value": 1824106.8207632212,
+            "unit": "ns",
+            "range": "± 7177.380019334514"
+          },
+          {
+            "name": "Libplanet.Benchmarks.DataModel.DataModelBenchmark.EncodeRawLeafModel",
+            "value": 1353321.568359375,
+            "unit": "ns",
+            "range": "± 5773.767739056391"
+          },
+          {
+            "name": "Libplanet.Benchmarks.DataModel.DataModelBenchmark.DecodeRootModel",
+            "value": 2608414.2640625,
+            "unit": "ns",
+            "range": "± 9009.055351164205"
+          },
+          {
+            "name": "Libplanet.Benchmarks.DataModel.DataModelBenchmark.DecodeLeafModel",
+            "value": 819310.6095703125,
+            "unit": "ns",
+            "range": "± 3567.501195913086"
+          },
+          {
+            "name": "Libplanet.Benchmarks.DataModel.DataModelBenchmark.DecodeRawLeafModel",
+            "value": 756483.3888811384,
+            "unit": "ns",
+            "range": "± 1099.3829024789138"
           }
         ]
       }
