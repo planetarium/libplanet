6f3d25939f89f495f43d239e9039cd99266fdd5d
github-action-benchmark <github@users.noreply.github.com>
2022-12-14 04:16:54 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 8761c9a0208a512406ee3b1bd32df33999dc622b

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1670991414513,
+  "repoUrl": "https://github.com/planetarium/libplanet",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "riemannulus@hitagi.moe",
+            "name": "Lee, Suho",
+            "username": "riemannulus"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8761c9a0208a512406ee3b1bd32df33999dc622b",
+          "message": "Merge pull request #2634 from riemannulus/fix/ci/benchmark/exclusive-options\n\nRemove exclusive option",
+          "timestamp": "2022-12-14T13:06:11+09:00",
+          "tree_id": "5e888d4168401483cf3971d9048e2cd22183e0a5",
+          "url": "https://github.com/planetarium/libplanet/commit/8761c9a0208a512406ee3b1bd32df33999dc622b"
+        },
+        "date": 1670991398852,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Libplanet.Benchmarks.BlockChain.ContainsBlock",
+            "value": 109702.61538461539,
+            "unit": "ns",
+            "range": "± 1478.7833703454528"
+          }
+        ]
+      }
+    ]
+  }
+}
