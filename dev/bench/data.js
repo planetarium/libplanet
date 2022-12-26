2c171adaa57adad39df1342fa4e3b5d6f62c8e06
github-action-benchmark <github@users.noreply.github.com>
2022-12-26 05:58:36 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for f1f6cb1586a17c51d905518df832e7f3f85d8e30

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1672034288224,
+  "lastUpdate": 1672034316246,
   "repoUrl": "https://github.com/planetarium/libplanet",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -2694,6 +2694,36 @@ window.BENCHMARK_DATA = {
             "value": 93914.28571428571,
             "unit": "ns",
             "range": "± 3020.8240847921793"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dev.moreal@gmail.com",
+            "name": "Lee Dogeon",
+            "username": "moreal"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f1f6cb1586a17c51d905518df832e7f3f85d8e30",
+          "message": "Merge pull request #2668 from moreal/fix-typo\n\nFix typo 'dail' → 'dial'",
+          "timestamp": "2022-12-26T14:46:22+09:00",
+          "tree_id": "7471606bfb8e0ff6f8ff2a2560b3cc10c096f64b",
+          "url": "https://github.com/planetarium/libplanet/commit/f1f6cb1586a17c51d905518df832e7f3f85d8e30"
+        },
+        "date": 1672034299997,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Libplanet.Benchmarks.BlockChain.ContainsBlock",
+            "value": 109925.5294117647,
+            "unit": "ns",
+            "range": "± 4446.218097902715"
           }
         ]
       }
