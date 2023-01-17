c3271775a1bfbf515d0406f6bb8aa143471054a6
Swen Mun <swen@planetariumhq.com>
2023-01-17 21:20:52 +0900

    Add a testcase about NetMQSocket leakage

--- Libplanet.Net.Tests/Transports/NetMQTransportTest.cs
+++ Libplanet.Net.Tests/Transports/NetMQTransportTest.cs
@@ -1,6 +1,11 @@
 #nullable disable
 using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 using Libplanet.Crypto;
+using Libplanet.Net.Messages;
 using Libplanet.Net.Transports;
 using NetMQ;
 using Serilog;
@@ -41,6 +46,78 @@ namespace Libplanet.Net.Tests.Transports
         ~NetMQTransportTest()
         {
             Dispose(false);
+        }
+
+        [SkippableFact]
+        public async Task SendMessageAsyncNetMQSocketLeak()
+        {
+            int previousMaxSocket = NetMQConfig.MaxSockets;
+
+            try
+            {
+                // An arbitrary number to fit one transport testing.
+                NetMQConfig.MaxSockets = 12;
+                NetMQTransport transport = await NetMQTransport.Create(
+                    new PrivateKey(),
+                    new AppProtocolVersionOptions(),
+                    new HostOptions(IPAddress.Loopback.ToString(), new IceServer[] { }, 0)
+                );
+                transport.ProcessMessageHandler.Register(
+                    async m =>
+                    {
+                        await transport.ReplyMessageAsync(
+                            new PongMsg
+                            {
+                                Identity = m.Identity,
+                            },
+                            CancellationToken.None
+                        );
+                    }
+                );
+                await InitializeAsync(transport);
+
+                string invalidHost = Guid.NewGuid().ToString();
+
+                // it isn't assertion for Libplanet codes, but to make sure that `invalidHost`
+                // really fails lookup before moving to the next step.
+                Assert.ThrowsAny<SocketException>(() =>
+                {
+                    Dns.GetHostEntry(invalidHost);
+                });
+                var invalidPeer = new BoundPeer(
+                    new PrivateKey().PublicKey,
+                    new DnsEndPoint(invalidHost, 0)
+                );
+
+                CommunicationFailException exc =
+                    await Assert.ThrowsAsync<CommunicationFailException>(
+                        () => transport.SendMessageAsync(
+                            invalidPeer,
+                            new PingMsg(),
+                            TimeSpan.FromSeconds(5),
+                            default
+                        )
+                    );
+
+                // Expecting SocketException about host resolving since `invalidPeer` has an
+                // invalid hostname
+                Assert.IsAssignableFrom<SocketException>(exc.InnerException);
+
+                // Check sending/receiving after exceptions exceeding NetMQConifg.MaxSockets.
+                Message reply = await transport.SendMessageAsync(
+                    transport.AsPeer,
+                    new PingMsg(),
+                    TimeSpan.FromSeconds(1),
+                    default
+                );
+                Assert.IsType<PongMsg>(reply);
+
+                await transport.StopAsync(TimeSpan.Zero, CancellationToken.None);
+            }
+            finally
+            {
+                NetMQConfig.MaxSockets = previousMaxSocket;
+            }
         }
 
         public void Dispose()
