6cf722fcffcc71ee878adc9477de43205eed4d57
Say Cheong <greymistcube@gmail.com>
2023-03-03 14:21:51 +0900

    [skip changelog] General update to logging

--- Libplanet.Net/Transports/NetMQTransport.cs
+++ Libplanet.Net/Transports/NetMQTransport.cs
@@ -737,8 +737,13 @@ namespace Libplanet.Net.Transports
                 _logger.Verbose(waitMsg);
                 MessageRequest req = await reader.ReadAsync(cancellationToken);
 #endif
+                string messageType = _messageCodec.ParseMessageType(req.Message, true).ToString();
                 long left = Interlocked.Decrement(ref _requestCount);
-                _logger.Information("Request taken; {Count} requests left", left);
+                _logger.Debug(
+                    "Request {Message} {RequestId} taken for processing; {Count} requests left",
+                    messageType,
+                    req.Id,
+                    left);
 
                 _ = SynchronizationContext.Current.PostAsync(
                     () => ProcessRequest(req, req.CancellationToken)
@@ -783,7 +788,11 @@ namespace Libplanet.Net.Transports
                 dealer.Options.Identity = req.Id.ToByteArray();
                 try
                 {
-                    _logger.Debug("Trying to connect {RequestId}", req.Id);
+                    _logger.Debug(
+                        "Trying to connect to {Peer} for request {Message} {RequestId}",
+                        req.Peer,
+                        messageType,
+                        req.Id);
                     dealer.Connect(await req.Peer.ResolveNetMQAddressAsync());
                     incrementedSocketCount = Interlocked.Increment(ref _socketCount);
                     _logger
