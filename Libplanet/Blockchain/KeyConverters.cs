bf8224e710511a3fa9bd6c938e3722c71b63d103
Say Cheong <greymistcube@gmail.com>
2023-03-25 10:37:43 +0900

    Improved KeyConverters performance

--- Libplanet/Blockchain/KeyConverters.cs
+++ Libplanet/Blockchain/KeyConverters.cs
@@ -6,16 +6,16 @@ namespace Libplanet.Blockchain
     {
         internal const string ValidatorSetKey = "___";
 
-        internal static string ToStateKey(Address address) => address.ToHex().ToLowerInvariant();
+        internal static string ToStateKey(Address address) => ByteUtil.Hex(address.ByteArray);
 
         internal static string ToFungibleAssetKey(Address address, Currency currency) =>
-            "_" + address.ToHex().ToLowerInvariant() +
-            "_" + ByteUtil.Hex(currency.Hash.ByteArray).ToLowerInvariant();
+            "_" + ByteUtil.Hex(address.ByteArray) +
+            "_" + ByteUtil.Hex(currency.Hash.ByteArray);
 
         internal static string ToFungibleAssetKey((Address, Currency) pair) =>
             ToFungibleAssetKey(pair.Item1, pair.Item2);
 
         internal static string ToTotalSupplyKey(Currency currency) =>
-            "__" + ByteUtil.Hex(currency.Hash.ByteArray).ToLowerInvariant();
+            "__" + ByteUtil.Hex(currency.Hash.ByteArray);
     }
 }
