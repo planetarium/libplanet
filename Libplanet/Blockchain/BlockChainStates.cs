49a8555cfbbb28ac03f9b44e572fee87963625ab
Say Cheong <greymistcube@gmail.com>
2023-03-26 21:52:29 +0900

    Cache during commit

--- Libplanet/Blockchain/BlockChainStates.cs
+++ Libplanet/Blockchain/BlockChainStates.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
@@ -210,6 +211,37 @@ namespace Libplanet.Blockchain
             }
 
             throw new IncompleteBlockStatesException(offset);
+        }
+
+        internal void CacheStates(Block<T> block, IReadOnlyList<ActionEvaluation> evaluations)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int count = 0;
+            IImmutableSet<Address> addresses = evaluations
+                .SelectMany(a => a.OutputStates.StateUpdatedAddresses)
+                .ToImmutableHashSet();
+            IAccountStateDelta? lastStates = evaluations.Count > 0
+                ? evaluations[evaluations.Count - 1].OutputStates
+                : null;
+            foreach (Address address in addresses)
+            {
+                if (lastStates?.GetState(address) is { } value)
+                {
+                    _stateCache.AddOrUpdate(new CacheKey(block.Hash, address), value);
+                    count++;
+                }
+            }
+
+            Log
+                .ForContext("Source", nameof(BlockChainStates<T>))
+                .Debug(
+                    "Took {DurationMs} ms to cache {ValueCount} values " +
+                    "for {AddressCount} addresses for {MethodName}()",
+                    stopwatch.ElapsedMilliseconds,
+                    count,
+                    addresses.Count,
+                    nameof(CacheStates));
         }
 
         internal void Bind(BlockChain<T> blockChain)
