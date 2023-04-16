c06b8f96d2c7130448d65a6e2ced8e389b2f06a5
Say Cheong <greymistcube@gmail.com>
2023-04-16 10:02:44 +0900

    Reject reorg for renderer validation

--- Libplanet/Blockchain/Renderers/Debug/ValidatingActionRenderer.cs
+++ Libplanet/Blockchain/Renderers/Debug/ValidatingActionRenderer.cs
@@ -1,12 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Libplanet.Action;
-using Libplanet.Blockchain.Policies;
 using Libplanet.Blocks;
-using Libplanet.Store;
-using Libplanet.Tx;
 
 namespace Libplanet.Blockchain.Renderers.Debug
 {
@@ -108,180 +103,6 @@ namespace Libplanet.Blockchain.Renderers.Debug
             Validate();
         }
 
-        private void ValidateReorgEnd(
-            Block<T> oldTip,
-            Block<T> newTip,
-            Block<T> branchpoint)
-        {
-            if (!(BlockChain is BlockChain<T> chain))
-            {
-                return;
-            }
-
-            IBlockPolicy<T> policy = chain.Policy;
-            IStore store = chain.Store;
-            InvalidRenderException<T> heterogeneousGenesisError =
-                Error(Records, "Reorg occurred from the chain with different genesis.");
-
-            List<IAction> expectedUnrenderedActions = new List<IAction>();
-            BlockHeader header = oldTip.Header;
-            IEnumerable<TxId> txIds = oldTip.Transactions.Select(tx => tx.Id);
-            while (!header.Hash.Equals(branchpoint.Hash))
-            {
-                if (policy.BlockAction is IAction blockAction)
-                {
-                    expectedUnrenderedActions.Add(blockAction);
-                }
-
-                IEnumerable<Transaction<T>> transactions = txIds.Select(store.GetTransaction<T>);
-
-                transactions = ActionEvaluator.OrderTxsForEvaluation(
-                    header.ProtocolVersion,
-                    transactions,
-                    header.PreEvaluationHash.ByteArray
-                ).Cast<Transaction<T>>();
-
-                expectedUnrenderedActions.AddRange(
-                    transactions.SelectMany(t =>
-                        t.SystemAction is { } sa ? new[] { sa } : t.CustomActions!.Cast<IAction>()
-                    ).Reverse()
-                );
-
-                BlockDigest prevDigest = store.GetBlockDigest(
-                    header.PreviousHash ?? throw heterogeneousGenesisError
-                ) ?? throw Error(Records, $"Failed to load block {header.PreviousHash}.");
-                header = prevDigest.GetHeader();
-                txIds = prevDigest.TxIds.Select(b => new TxId(b.ToBuilder().ToArray()));
-            }
-
-            IEnumerable<IAction> expectedRenderedActionsBuffer = new List<IAction>();
-            header = newTip.Header;
-            txIds = newTip.Transactions.Select(tx => tx.Id);
-            while (!header.Hash.Equals(branchpoint.Hash))
-            {
-                IEnumerable<Transaction<T>> transactions = txIds.Select(store.GetTransaction<T>);
-                transactions = ActionEvaluator.OrderTxsForEvaluation(
-                    header.ProtocolVersion,
-                    transactions,
-                    header.PreEvaluationHash.ByteArray
-                ).Cast<Transaction<T>>();
-                IEnumerable<IAction> actions = transactions.SelectMany(t =>
-                    t.SystemAction is { } sa ? new[] { sa } : t.CustomActions!.Cast<IAction>()
-                );
-                if (policy.BlockAction is IAction blockAction)
-                {
-#if NET472 || NET471 || NET47 || NET462 || NET461
-                    // Even though .NET Framework 4.6.1 or higher supports .NET Standard 2.0,
-                    // versions lower than 4.8 lacks Enumerable.Append(IEnumerable<T>, T) method.
-                    actions = actions.Concat(new IAction[] { blockAction });
-#else
-#pragma warning disable PC002
-                    actions = actions.Append(blockAction);
-#pragma warning restore PC002
-#endif
-                }
-
-                expectedRenderedActionsBuffer = actions.Concat(expectedRenderedActionsBuffer);
-                BlockDigest prevDigest = store.GetBlockDigest(
-                    header.PreviousHash ?? throw heterogeneousGenesisError
-                ) ?? throw Error(Records, $"Failed to load block {header.PreviousHash}.");
-                header = prevDigest.GetHeader();
-                txIds = prevDigest.TxIds.Select(b => new TxId(b.ToBuilder().ToArray()));
-            }
-
-            IAction[] expectedRenderedActions = expectedRenderedActionsBuffer.ToArray();
-            List<IAction> actualRenderedActions = new List<IAction>();
-            List<IAction> actualUnrenderedActions = new List<IAction>();
-            foreach (var record in Records.Reverse())
-            {
-                if (record is RenderRecord<T>.Reorg b && b.Begin)
-                {
-                    break;
-                }
-
-                if (record is RenderRecord<T>.ActionBase a)
-                {
-                    if (a.Render)
-                    {
-                        actualRenderedActions.Add(a.Action);
-                    }
-                    else
-                    {
-                        actualUnrenderedActions.Add(a.Action);
-                    }
-                }
-            }
-
-            actualRenderedActions.Reverse();
-            actualUnrenderedActions.Reverse();
-
-            string ReprAction(IAction? action)
-            {
-                if (action is null)
-                {
-                    return "[N/A]";
-                }
-
-                return action.PlainValue.Inspect(loadAll: true)
-                    .Replace(" \n ", " ")
-                    .Replace(" \n", " ")
-                    .Replace("\n ", " ")
-                    .Replace("\n", " ");
-            }
-
-            string MakeErrorMessage(string prefix, IList<IAction> expected, IList<IAction> actual)
-            {
-                int expectN = expected.Count;
-                int actualN = actual.Count;
-                if (expectN != actualN)
-                {
-                    prefix += $" (expected: {expectN} actions, actual: {actualN} actions):";
-                }
-
-                var buffer = new StringBuilder();
-                for (int i = 0, count = Math.Max(expectN, actualN); i < count; i++)
-                {
-                    IAction? e = i < expectN ? expected[i] : null;
-                    IAction? a = i < actualN ? actual[i] : null;
-                    if (!(e is null || a is null) && e.PlainValue.Equals(a.PlainValue))
-                    {
-                        buffer.Append($"\n\t  {ReprAction(e)}");
-                    }
-                    else
-                    {
-                        buffer.Append($"\n\tE {ReprAction(e)}");
-                        buffer.Append($"\n\tA {ReprAction(a)}");
-                    }
-                }
-
-                return $"{prefix}:{buffer}";
-            }
-
-            if (!actualUnrenderedActions.Select(a => a.PlainValue)
-                    .SequenceEqual(expectedUnrenderedActions.Select(a => a.PlainValue)))
-            {
-                const string message =
-                    "The unrender action records do not match with actions in the block when " +
-                    "reorg occurred";
-                throw Error(
-                    Records,
-                    MakeErrorMessage(message, expectedUnrenderedActions, actualUnrenderedActions)
-                );
-            }
-
-            if (!actualRenderedActions.Select(a => a.PlainValue)
-                    .SequenceEqual(expectedRenderedActions.Select(a => a.PlainValue)))
-            {
-                const string message =
-                    "The render action record does not match with actions in the block when " +
-                    "reorg occurred";
-                throw Error(
-                    Records,
-                    MakeErrorMessage(message, expectedRenderedActions, actualRenderedActions)
-                );
-            }
-        }
-
         private void Validate()
         {
             var state = RenderState.Ready;
@@ -299,7 +120,9 @@ namespace Libplanet.Blockchain.Renderers.Debug
                 {
                     case RenderState.Ready:
                     {
+#pragma warning disable S2589
                         if (!(reorgState is null && blockState is null))
+#pragma warning restore S2589
                         {
                             throw BadRenderExc(
                                 $"Unexpected reorg/block states: {reorgState}/{blockState}."
@@ -309,9 +132,7 @@ namespace Libplanet.Blockchain.Renderers.Debug
                         {
                             if (blockBase is RenderRecord<T>.Reorg reorg)
                             {
-                                reorgState = reorg;
-                                state = RenderState.Reorg;
-                                break;
+                                throw BadRenderExc("Reorg is deprecated");
                             }
                             else if (blockBase is RenderRecord<T>.Block block)
                             {
@@ -326,54 +147,7 @@ namespace Libplanet.Blockchain.Renderers.Debug
 
                     case RenderState.Reorg:
                     {
-#pragma warning disable S2589
-                        if (reorgState is null || !(blockState is null))
-#pragma warning restore S2589
-                        {
-                            throw BadRenderExc(
-                                $"Unexpected reorg/block states: {reorgState}/{blockState}."
-                            );
-                        }
-                        else if (record is RenderRecord<T>.Block block && block.Begin)
-                        {
-                            if (block.OldTip != reorgState.OldTip ||
-                                block.NewTip != reorgState.NewTip)
-                            {
-                                throw BadRenderExc(
-                                    $"RenderReorg and {nameof(IRenderer<T>.RenderBlock)} " +
-                                    "should have the same oldTip and newTip."
-                                );
-                            }
-
-                            blockState = block;
-                            state = RenderState.Block;
-                            break;
-                        }
-                        else if (record is RenderRecord<T>.ActionBase actionBase &&
-                                 actionBase.Unrender)
-                        {
-                            long idx = actionBase.Context.BlockIndex;
-                            long minIdx = reorgState.Branchpoint.Index + 1;
-                            long maxIdx = previousActionBlockIndex < 0
-                                ? reorgState.OldTip.Index
-                                : previousActionBlockIndex;
-                            if (idx < minIdx || idx > maxIdx)
-                            {
-                                throw BadRenderExc(
-                                    "An action is from a block which has an unexpected index " +
-                                    $"#{idx} (expected min: #{minIdx}; max: #{maxIdx})."
-                                );
-                            }
-
-                            previousActionBlockIndex = idx;
-                            break;
-                        }
-
-                        throw BadRenderExc(
-                            $"Expected {nameof(IRenderer<T>.RenderBlock)} or " +
-                            $"{nameof(IActionRenderer<T>.UnrenderAction)} or " +
-                            $"{nameof(IActionRenderer<T>.UnrenderActionError)}."
-                        );
+                        throw BadRenderExc("Reorg is deprecated");
                     }
 
                     case RenderState.Block:
@@ -439,31 +213,19 @@ namespace Libplanet.Blockchain.Renderers.Debug
 
                     case RenderState.BlockEnd:
                     {
-#pragma warning disable S2589
+#pragma warning disable S2583, S2589
                         if (reorgState is null || !(blockState is null))
-#pragma warning restore S2589
+#pragma warning restore S2583, S2589
                         {
                             throw BadRenderExc(
-                                $"Unexpected reorg/block states: {reorgState}/{blockState}."
-                            );
+                                $"Unexpected reorg/block states: {reorgState}/{blockState}.");
                         }
                         else if (record is RenderRecord<T>.Reorg reorg && reorg.End)
                         {
-                            if (reorg.OldTip != reorgState.OldTip ||
-                                reorg.NewTip != reorgState.NewTip ||
-                                reorg.Branchpoint != reorgState.Branchpoint)
-                            {
-                                throw BadRenderExc(
-                                    "RenderReorgEnd should match to RenderReorg; " +
-                                    "they should have the same oldTip, newTip, and branchpoint.");
-                            }
-
-                            state = RenderState.Ready;
-                            reorgState = null;
-                            break;
+                            throw BadRenderExc("Reorg is deprecated");
                         }
 
-                        throw BadRenderExc("Expected RenderReorgEnd");
+                        throw BadRenderExc($"Unexpected record encountered: {record}");
                     }
                 }
             }
