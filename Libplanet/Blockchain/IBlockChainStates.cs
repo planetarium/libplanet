e2928b2445fa84b3d769723bfc817cc6cc7384ab
Say Cheong <greymistcube@gmail.com>
2023-06-14 16:31:23 +0900

    Updated docs

--- Libplanet/Blockchain/IBlockChainStates.cs
+++ Libplanet/Blockchain/IBlockChainStates.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using Bencodex.Types;
 using Libplanet.Assets;
 using Libplanet.Blocks;
 using Libplanet.Consensus;
 using Libplanet.State;
+using Libplanet.Store;
 
 namespace Libplanet.Blockchain
 {
@@ -23,6 +25,18 @@ namespace Libplanet.Blockchain
         /// Associated values are ordered in the same way to the corresponding
         /// <paramref name="addresses"/>.  Absent states are represented as <see langword="null"/>.
         /// </returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="offset"/> is not
+        /// <see langword="null"/> and one of the following is true.
+        /// <list type="bullet">
+        ///     <item><description>
+        ///         Corresponding <see cref="Block"/> is not found in the <see cref="IStore"/>.
+        ///     </description></item>
+        ///     <item><description>
+        ///         Corresponding <see cref="Block"/> is found but its state root is not found
+        ///         in the <see cref="IStateStore"/>.
+        ///     </description></item>
+        /// </list>
+        /// </exception>
         IReadOnlyList<IValue?> GetStates(
             IReadOnlyList<Address> addresses,
             BlockHash? offset);
@@ -39,6 +53,18 @@ namespace Libplanet.Blockchain
         /// at <paramref name="offset"/>.  If absent, returns 0 <see cref="FungibleAssetValue"/>
         /// for <paramref name="currency"/>.
         /// </returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="offset"/> is not
+        /// <see langword="null"/> and one of the following is true.
+        /// <list type="bullet">
+        ///     <item><description>
+        ///         Corresponding <see cref="Block"/> is not found in the <see cref="IStore"/>.
+        ///     </description></item>
+        ///     <item><description>
+        ///         Corresponding <see cref="Block"/> is found but its state root is not found
+        ///         in the <see cref="IStateStore"/>.
+        ///     </description></item>
+        /// </list>
+        /// </exception>
         FungibleAssetValue GetBalance(
             Address address,
             Currency currency,
@@ -55,9 +81,21 @@ namespace Libplanet.Blockchain
         /// <paramref name="offset"/> in <see cref="FungibleAssetValue"/>.
         /// If absent, returns 0 <see cref="FungibleAssetValue"/>
         /// for <paramref name="currency"/>.</returns>
-        /// <exception cref="TotalSupplyNotTrackableException">Thrown if
+        /// <exception cref="TotalSupplyNotTrackableException">Thrown when
         /// given <paramref name="currency"/>'s <see cref="Currency.TotalSupplyTrackable"/>
         /// is <see langword="false"/>.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="offset"/> is not
+        /// <see langword="null"/> and one of the following is true.
+        /// <list type="bullet">
+        ///     <item><description>
+        ///         Corresponding <see cref="Block"/> is not found in the <see cref="IStore"/>.
+        ///     </description></item>
+        ///     <item><description>
+        ///         Corresponding <see cref="Block"/> is found but its state root is not found
+        ///         in the <see cref="IStateStore"/>.
+        ///     </description></item>
+        /// </list>
+        /// </exception>
         FungibleAssetValue GetTotalSupply(
             Currency currency,
             BlockHash? offset);
@@ -71,6 +109,18 @@ namespace Libplanet.Blockchain
         /// <returns>The validator set of type <see cref="ValidatorSet"/> at
         /// <paramref name="offset"/>.
         /// </returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="offset"/> is not
+        /// <see langword="null"/> and one of the following is true.
+        /// <list type="bullet">
+        ///     <item><description>
+        ///         Corresponding <see cref="Block"/> is not found in the <see cref="IStore"/>.
+        ///     </description></item>
+        ///     <item><description>
+        ///         Corresponding <see cref="Block"/> is found but its state root is not found
+        ///         in the <see cref="IStateStore"/>.
+        ///     </description></item>
+        /// </list>
+        /// </exception>
         ValidatorSet GetValidatorSet(BlockHash? offset);
     }
 }
