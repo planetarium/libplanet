8f8285bfaa1efd53d5c4febcb10d4e6c3245755b
ilgyu <ilgyu@planetariumhq.com>
2023-07-04 18:29:01 +0900

    Add spam filter logic on communicator

--- Libplanet.Net/Consensus/GossipConsensusMessageCommunicator.cs
+++ Libplanet.Net/Consensus/GossipConsensusMessageCommunicator.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using Libplanet.Net.Messages;
 using Libplanet.Net.Transports;
+using Serilog;
 
 namespace Libplanet.Net.Consensus
 {
@@ -10,8 +12,10 @@ namespace Libplanet.Net.Consensus
     /// </summary>
     public class GossipConsensusMessageCommunicator : IConsensusMessageCommunicator
     {
+        private readonly ILogger _logger;
         private long _height;
         private int _round;
+        private Dictionary<BoundPeer, HashSet<int>> _peerCatchupRounds;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="GossipConsensusMessageCommunicator"/>
@@ -32,9 +36,21 @@ namespace Libplanet.Net.Consensus
             Action<MessageContent> processMessage)
         {
             Gossip = new Gossip(
-                consensusTransport, validatorPeers, seedPeers, _ => { }, processMessage);
+                consensusTransport,
+                validatorPeers,
+                seedPeers,
+                ValidateMessageToReceive,
+                ValidateMessageToSend,
+                processMessage);
             _height = 0;
             _round = 0;
+            _peerCatchupRounds = new Dictionary<BoundPeer, HashSet<int>>();
+
+            _logger = Log
+                .ForContext("Tag", "Consensus")
+                .ForContext("SubTag", "ConsensusMessageCommunicator")
+                .ForContext<GossipConsensusMessageCommunicator>()
+                .ForContext("Source", nameof(GossipConsensusMessageCommunicator));
         }
 
         /// <summary>
@@ -46,16 +62,78 @@ namespace Libplanet.Net.Consensus
         public void PublishMessage(ConsensusMsg message)
             => Gossip.PublishMessage(message);
 
+        /// <inheritdoc/>
         public void OnStartHeight(long height)
         {
             _height = height;
             Gossip.ClearDenySet();
         }
 
+        /// <inheritdoc/>
         public void OnStartRound(int round)
         {
             _round = round;
             Gossip.ClearCache();
+        }
+
+        /// <summary>
+        /// Message validator to be called on <see cref="Gossip.HandleMessageAsync"/>.
+        /// This will be set as parameter of <see cref="Gossip"/>, and will validate
+        /// <see cref="Message"/>s before it's added to the <see cref="Gossip._cache"/>.
+        /// </summary>
+        /// <param name="message"><see cref="Message"/> to validate.</param>
+        private void ValidateMessageToReceive(Message message)
+        {
+            if (message.Content is ConsensusVoteMsg voteMsg)
+            {
+                FilterSpam(voteMsg, message.Remote);
+            }
+        }
+
+        /// <summary>
+        /// Message content validator to be called on <see cref="Gossip.HandleWantAsync"/>.
+        /// This will be set as parameter of <see cref="Gossip"/>, and will validate
+        /// <see cref="Message"/>s before respond to peer's <see cref="WantMessage"/>.
+        /// </summary>
+        /// <param name="content"><see cref="MessageContent"/> to validate.</param>
+        private void ValidateMessageToSend(MessageContent content)
+        {
+            if (content is ConsensusVoteMsg voteMsg && voteMsg.Round > _round)
+            {
+                throw new InvalidConsensusMessageException(
+                    $"Cannot send vote of round higher than context", voteMsg);
+            }
+        }
+
+        /// <summary>
+        /// Spam filter logic for higher round <see cref="ConsensusVoteMsg"/>s.
+        /// </summary>
+        /// <param name="voteMsg"><see cref="Message"/> to filter.</param>
+        /// <param name="peer"><see cref="BoundPeer"/> who sent <paramref name="voteMsg"/>.
+        /// </param>
+        private void FilterSpam(ConsensusVoteMsg voteMsg, BoundPeer peer)
+        {
+            if (voteMsg.Round > _round)
+            {
+                if (!_peerCatchupRounds.ContainsKey(peer))
+                {
+                    _peerCatchupRounds[peer] = new HashSet<int>();
+                }
+
+                HashSet<int> rounds = _peerCatchupRounds[peer];
+                if (rounds.Count < 2)
+                {
+                    rounds.Add(voteMsg.Round);
+                    _peerCatchupRounds[peer] = rounds;
+                }
+                else
+                {
+                    Gossip.DenyPeer(peer);
+                    throw new InvalidConsensusMessageException(
+                        $"Repetitively found heigher rounds, add {peer} to deny set",
+                        voteMsg);
+                }
+            }
         }
     }
 }
