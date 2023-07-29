49b0c5195840094a706c93fb185b65ce8b048450
ilgyu <ilgyu.yi@gmail.com>
2023-07-29 16:21:32 +0900

    test: Add WorldTest

---
+++ Libplanet.Action.Tests/Mocks/MockWorldState.cs
@@ -0,0 +1,29 @@
+using System.Collections.Immutable;
+using Libplanet.Action.State;
+using Libplanet.Crypto;
+
+namespace Libplanet.Action.Tests.Mocks
+{
+    public class MockWorldState : IWorldState
+    {
+        private readonly IImmutableDictionary<Address, IAccount> _accounts;
+
+        public MockWorldState(IImmutableDictionary<Address, IAccount> accounts)
+        {
+            _accounts = accounts;
+        }
+
+        public MockWorldState()
+        {
+            _accounts = ImmutableDictionary<Address, IAccount>.Empty;
+        }
+
+        public bool Legacy { get; private set; }
+
+        IAccount? IWorldState.GetAccount(Address address)
+        {
+            _accounts.TryGetValue(address, out IAccount? account);
+            return account;
+        }
+    }
+}
---
+++ Libplanet.Action.Tests/State/WorldTest.cs
@@ -0,0 +1,110 @@
+using System.Collections.Immutable;
+using Bencodex.Types;
+using Libplanet.Action.Tests.Mocks;
+using Libplanet.Crypto;
+using Xunit;
+
+namespace Libplanet.Action.State.Tests
+{
+    public class WorldTest
+    {
+        private readonly System.Random _random;
+        private readonly byte[] _randomAddrBytes;
+
+        public WorldTest()
+        {
+            _random = new System.Random();
+            _randomAddrBytes = new byte[20];
+        }
+
+        public Address RandomAddress
+        {
+            get
+            {
+                _random.NextBytes(_randomAddrBytes);
+                return new Address(_randomAddrBytes);
+            }
+        }
+
+        [Fact]
+        public void Create()
+        {
+            IAccount mockAccount = Account.Create(
+                MockAccountState.Empty.SetState(RandomAddress, new Integer(1)));
+            Address accountAddress = RandomAddress;
+
+            MockWorldState baseWorld = new MockWorldState(
+                ImmutableDictionary<Address, IAccount>.Empty.Add(accountAddress, mockAccount));
+            IWorld world = World.Create(baseWorld);
+            Assert.IsType<World>(world);
+            // GetAccount() will retrieve account from World._baseState
+            Assert.Equal(mockAccount, world.GetAccount(accountAddress));
+        }
+
+        [Fact]
+        public void SetAccount()
+        {
+            IAccount mockAccount = Account.Create(
+                MockAccountState.Empty.SetState(RandomAddress, new Integer(1)));
+            Address accountAddress = RandomAddress;
+
+            MockWorldState baseWorld = new MockWorldState();
+            IWorld world = World.Create(baseWorld);
+            Assert.Null(world.GetAccount(accountAddress));
+
+            // TestGetAccount() will retrieve account from World.Delta
+            world = world.SetAccount(accountAddress, mockAccount);
+            Assert.Single(world.Delta.UpdatedAccounts);
+            Assert.Equal(accountAddress, world.Delta.UpdatedAccounts.First());
+            Assert.Equal(mockAccount, world.GetAccount(accountAddress));
+        }
+
+        [Fact]
+        public void Flush()
+        {
+            MockWorldState baseWorld = new MockWorldState();
+            IWorld world = World.Create(baseWorld);
+
+            // Before flush, updated accounts exists in World.Delta.
+            IAccount emptyAccount = Account.Create(MockAccountState.Empty);
+            Address accountAddress = RandomAddress;
+            world = world.SetAccount(accountAddress, emptyAccount);
+            Assert.Single(world.Delta.UpdatedAccounts);
+            Assert.Equal(accountAddress, world.Delta.UpdatedAccounts.First());
+
+            // After flush, World.Delta moves to World._baseState.Delta
+            world = World.Flush(world);
+            Assert.Empty(world.Delta.UpdatedAccounts);
+            Assert.NotNull(world.GetAccount(accountAddress));
+        }
+
+        [Fact]
+        public void LegacyFlag()
+        {
+            // New World have legacy account is non-legacy world
+            MockWorldState baseWorldNonLegacy = new MockWorldState(
+                ImmutableDictionary<Address, IAccount>.Empty.Add(
+                    ReservedAddresses.LegacyAccount, Account.Create(MockAccountState.Empty)));
+            IWorld world = World.Create(baseWorldNonLegacy);
+            Assert.False(world.Legacy);
+
+            // New world does not have legacy account is legacy world
+            MockWorldState baseWorld = new MockWorldState();
+            world = World.Create(baseWorld);
+            Assert.True(world.Legacy);
+
+            // world from SetAccount with legacy account address is legacy world
+            IAccount account = Account.Create(MockAccountState.Empty);
+            world = world.SetAccount(ReservedAddresses.LegacyAccount, account);
+            Assert.True(world.Legacy);
+
+            // World from SetAccount with non-legacy account address is non-legacy world
+            world = world.SetAccount(RandomAddress, account);
+            Assert.False(world.Legacy);
+
+            // Once world has been set as non-legacy, it cannot recover to legacy world
+            world = world.SetAccount(ReservedAddresses.LegacyAccount, account);
+            Assert.False(world.Legacy);
+        }
+    }
+}
