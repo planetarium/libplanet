71f5a6b340c67476d95edb5e831f950eeb68721a
Say Cheong <greymistcube@gmail.com>
2023-09-15 17:43:48 +0900

    Added test

---
+++ Libplanet.Tests/Action/AccountDiffTest.cs
@@ -0,0 +1,172 @@
+using System.Collections.Generic;
+using Bencodex.Types;
+using Libplanet.Action;
+using Libplanet.Action.State;
+using Libplanet.Crypto;
+using Libplanet.Store;
+using Libplanet.Store.Trie;
+using Libplanet.Types.Assets;
+using Libplanet.Types.Blocks;
+using Libplanet.Types.Consensus;
+using Xunit;
+
+namespace Libplanet.Tests.Action
+{
+    public class AccountDiffTest
+    {
+        public AccountDiffTest()
+        {
+            USD = Currency.Uncapped("USD", 2, null);
+            KRW = Currency.Uncapped("KRW", 0, null);
+            JPY = Currency.Uncapped("JPY", 0, null);
+        }
+
+        public Currency USD { get; }
+
+        public Currency KRW { get; }
+
+        public Currency JPY { get; }
+
+        [Fact]
+        public void EmptyAccountStateSource()
+        {
+            IStateStore stateStore = new TrieStateStore(new MemoryKeyValueStore());
+            ITrie targetTrie = stateStore.GetStateRoot(null);
+            ITrie sourceTrie = stateStore.GetStateRoot(null);
+
+            AccountDiff diff = AccountDiff.Create(targetTrie, sourceTrie);
+            Assert.Empty(diff.StateDiffs);
+            Assert.Empty(diff.FungibleAssetValueDiffs);
+            Assert.Empty(diff.TotalSupplyDiffs);
+            Assert.Null(diff.ValidatorSetDiff);
+
+            IAccount targetAccount = new Account(new AccountState(targetTrie));
+            PrivateKey signer = new PrivateKey();
+            IActionContext context = CreateActionContext(signer.ToAddress(), targetTrie);
+            targetAccount = targetAccount.MintAsset(
+                context, signer.ToAddress(), new FungibleAssetValue(USD, 123, 45));
+            targetAccount = targetAccount.SetState(signer.ToAddress(), new Text("Foo"));
+
+            targetTrie = Commit(stateStore, targetTrie, targetAccount.Delta);
+
+            diff = AccountDiff.Create(targetTrie, sourceTrie);
+            Assert.Empty(diff.StateDiffs);
+            Assert.Empty(diff.FungibleAssetValueDiffs);
+            Assert.Empty(diff.TotalSupplyDiffs);
+            Assert.Null(diff.ValidatorSetDiff);
+        }
+
+        [Fact]
+        public void Diff()
+        {
+            IStateStore stateStore = new TrieStateStore(new MemoryKeyValueStore());
+            ITrie targetTrie = stateStore.GetStateRoot(null);
+            ITrie sourceTrie = stateStore.GetStateRoot(null);
+
+            Address addr1 = new Address(TestUtils.GetRandomBytes(Address.Size));
+            Address addr2 = new Address(TestUtils.GetRandomBytes(Address.Size));
+            Address addr3 = new Address(TestUtils.GetRandomBytes(Address.Size));
+
+            AccountDiff diff = AccountDiff.Create(targetTrie, sourceTrie);
+            Assert.Empty(diff.StateDiffs);
+            Assert.Empty(diff.FungibleAssetValueDiffs);
+            Assert.Empty(diff.TotalSupplyDiffs);
+            Assert.Null(diff.ValidatorSetDiff);
+
+            IAccount targetAccount = new Account(new AccountState(targetTrie));
+            PrivateKey signer = new PrivateKey();
+            IActionContext context = CreateActionContext(signer.ToAddress(), targetTrie);
+            targetAccount = targetAccount.SetState(addr1, new Text("One"));
+            targetAccount = targetAccount.SetState(addr2, new Text("Two"));
+            targetAccount = targetAccount.MintAsset(
+                context, signer.ToAddress(), new FungibleAssetValue(USD, 123, 45));
+
+            targetTrie = Commit(stateStore, targetTrie, targetAccount.Delta);
+            sourceTrie = targetTrie;
+
+            IAccount sourceAccount = new Account(new AccountState(sourceTrie));
+            sourceAccount = sourceAccount.SetState(addr2, new Text("Two_"));
+            sourceAccount = sourceAccount.SetState(addr3, new Text("Three"));
+            sourceAccount = sourceAccount.MintAsset(
+                context, signer.ToAddress(), new FungibleAssetValue(USD, 456, 78));
+            sourceAccount = sourceAccount.MintAsset(
+                context, signer.ToAddress(), new FungibleAssetValue(KRW, 10, 0));
+            sourceAccount = sourceAccount.BurnAsset(
+                context, signer.ToAddress(), new FungibleAssetValue(KRW, 10, 0));
+            sourceAccount = sourceAccount.MintAsset(
+                context, signer.ToAddress(), new FungibleAssetValue(JPY, 321, 0));
+            sourceAccount = sourceAccount.SetValidator(new Validator(signer.PublicKey, 1));
+
+            sourceTrie = Commit(stateStore, sourceTrie, sourceAccount.Delta);
+
+            diff = AccountDiff.Create(targetTrie, sourceTrie);
+            Assert.Equal(2, diff.StateDiffs.Count);
+            Assert.Equal((new Text("Two"), new Text("Two_")), diff.StateDiffs[addr2]);
+            Assert.Equal((null, new Text("Three")), diff.StateDiffs[addr3]);
+
+            Assert.Equal(2, diff.FungibleAssetValueDiffs.Count);    // KRW is treated as unchanged
+            Assert.Equal(
+                (new Integer(12345), new Integer(12345 + 45678)),
+                diff.FungibleAssetValueDiffs[(signer.ToAddress(), USD.Hash)]);
+            Assert.Equal(
+                (new Integer(0), new Integer(321)),
+                diff.FungibleAssetValueDiffs[(signer.ToAddress(), JPY.Hash)]);
+
+            Assert.Equal(2, diff.TotalSupplyDiffs.Count);           // KRW is treated as unchanged
+            Assert.Equal(
+                (new Integer(12345), new Integer(12345 + 45678)),
+                diff.TotalSupplyDiffs[USD.Hash]);
+            Assert.Equal(
+                (new Integer(0), new Integer(321)),
+                diff.TotalSupplyDiffs[JPY.Hash]);
+
+            Assert.Equal(
+                (
+                    new ValidatorSet(),
+                    new ValidatorSet(new List<Validator>() { new Validator(signer.PublicKey, 1) })
+                ),
+                diff.ValidatorSetDiff);
+
+            diff = AccountDiff.Create(sourceTrie, targetTrie);
+            Assert.Single(diff.StateDiffs);                 // Note addr3 is not tracked
+            Assert.Equal((new Text("Two_"), new Text("Two")), diff.StateDiffs[addr2]);
+            Assert.Single(diff.FungibleAssetValueDiffs);    // Only USD is tracked
+            Assert.Equal(
+                (new Integer(12345 + 45678), new Integer(12345)),
+                diff.FungibleAssetValueDiffs[(signer.ToAddress(), USD.Hash)]);
+            Assert.Single(diff.TotalSupplyDiffs);           // Only USD is tracked
+            Assert.Equal(
+                (new Integer(12345 + 45678), new Integer(12345)),
+                diff.TotalSupplyDiffs[USD.Hash]);
+            Assert.Null(diff.ValidatorSetDiff);             // Note ValidatorSet is not tracked
+        }
+
+        public IActionContext CreateActionContext(Address signer, ITrie trie) =>
+            new ActionContext(
+                signer,
+                null,
+                signer,
+                0,
+                Block.CurrentProtocolVersion,
+                new Account(new AccountState(trie)),
+                0,
+                0,
+                false);
+
+        public ITrie Commit(
+            IStateStore stateStore,
+            ITrie baseTrie,
+            IAccountDelta accountDelta)
+        {
+            var trie = baseTrie;
+            var rawDelta = accountDelta.ToRawDelta();
+
+            foreach (var kv in rawDelta)
+            {
+                trie = trie.Set(kv.Key, kv.Value);
+            }
+
+            return stateStore.Commit(trie);
+        }
+    }
+}
