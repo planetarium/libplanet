e813b55e66c57a523b0a5266f79f5f7032da105b
Say Cheong <greymistcube@gmail.com>
2023-09-18 10:30:15 +0900

    Introduce IActionRenderContext

---
+++ Libplanet.Action/IActionRenderContext.cs
@@ -0,0 +1,86 @@
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
+using Libplanet.Common;
+using Libplanet.Crypto;
+using Libplanet.Types.Blocks;
+using Libplanet.Types.Tx;
+
+namespace Libplanet.Action
+{
+    /// <summary>
+    /// Contextual data determined by a transaction and a block for rendering.
+    /// </summary>
+    public interface IActionRenderContext
+    {
+        /// <summary>
+        /// The <see cref="Transaction.Signer"/> of the <see cref="Transaction"/> that contains
+        /// the <see cref="IAction"/> to be executed.  If the <see cref="IAction"/> is
+        /// not part of a <see cref="Transaction"/>, e.g. <see cref="IBlockPolicy.BlockAction"/>,
+        /// this is set to <see cref="Block.Miner"/> instead.
+        /// </summary>
+        [Pure]
+        Address Signer { get; }
+
+        /// <summary>
+        /// The <see cref="Transaction.Id"/> of the <see cref="Transaction"/> that contains
+        /// the <see cref="IAction"/>.  If the <see cref="IAction"/> is not part of
+        /// a <see cref="Transaction"/>, e.g. <see cref="IBlockPolicy.BlockAction"/>,
+        /// this is set to <see langword="null"/>.
+        /// </summary>
+        [Pure]
+        TxId? TxId { get; }
+
+        /// <summary>
+        /// The <see cref="Block.Miner"/> of the <see cref="Block"/> that contains
+        /// the <see cref="IAction"/>.
+        /// </summary>
+        [Pure]
+        Address Miner { get; }
+
+        /// <summary>
+        /// The <see cref="Block.Index"/> of the <see cref="Block"/> that contains
+        /// the <see cref="IAction"/>.
+        /// </summary>
+        [Pure]
+        long BlockIndex { get; }
+
+        /// <summary>
+        /// The <see cref="Block.ProtocolVersion"/> of the <see cref="Block"/> that contains
+        /// the <see cref="IAction"/>.
+        /// </summary>
+        [Pure]
+        int BlockProtocolVersion { get; }
+
+        /// <summary>
+        /// Whether an <see cref="IAction"/> is being executed during
+        /// &#x201c;rehearsal mode&#x201d;, that there is nothing
+        /// in <see cref="PreviousState"/>.
+        /// </summary>
+        [Pure]
+        bool Rehearsal { get; }
+
+        /// <summary>
+        /// The state root hash of the previous state.
+        /// </summary>
+        [Pure]
+        HashDigest<SHA256> PreviousState { get; }
+
+        /// <summary>
+        /// An initialized pseudorandom number generator.  Its seed (state)
+        /// is determined by a block and a transaction, which is
+        /// deterministic so that every node can replay the same action and
+        /// then reproduce the same result, while neither a single block miner
+        /// nor a single transaction signer can predict the result and cheat.
+        /// </summary>
+        /// <returns>A random object that shares interface mostly equivalent
+        /// to <see cref="System.Random"/>.</returns>
+        IRandom Random { get; }
+
+        /// <summary>
+        /// Whether this action is executed as a block action.
+        /// <see langword="false"/> if it belongs to a transaction.
+        /// </summary>
+        [Pure]
+        bool BlockAction { get; }
+    }
+}
