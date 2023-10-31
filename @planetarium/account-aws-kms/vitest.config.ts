e1c134eb179d05060b8275c62ab87dd03e5faf60
Akamig <akamig@posteo.net>
2023-10-31 11:11:19 +0900

    Use longer timeout for KMS test

--- @planetarium/account-aws-kms/vitest.config.ts
+++ @planetarium/account-aws-kms/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     cache: false,
-    testTimeout: 30000,
+    testTimeout: 45000,
     alias: {
       "#crypto": "./src/crypto/node.ts",
     },
