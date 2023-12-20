3edb6a9fc98022381bc83521d4c021a338012fcc
Ko Chanhyuck <limeelbee@gmail.com>
2023-12-20 19:08:42 +0900

    feat: use seed in kademlia peer discovery

--- Libplanet.Net/Protocols/IProtocol.cs
+++ Libplanet.Net/Protocols/IProtocol.cs
@@ -69,6 +69,19 @@ namespace Libplanet.Net.Protocols
         Task RebuildConnectionAsync(int depth, CancellationToken cancellationToken);
 
         /// <summary>
+        /// Reconstructs network connection between peers on network using seed peers.
+        /// </summary>
+        /// <param name="seedPeers">The list of the see peers.</param>
+        /// <param name="depth">Recursive operation depth to search peers from network.</param>
+        /// <param name="cancellationToken">A cancellation token used to propagate notification
+        /// that this operation should be canceled.</param>
+        /// <returns>An awaitable task without value.</returns>
+        Task RebuildConnectionAsync(
+            IEnumerable<BoundPeer> seedPeers,
+            int depth,
+            CancellationToken cancellationToken);
+
+        /// <summary>
         /// Checks the <see cref="KBucket"/> in the <see cref="RoutingTable"/> and if
         /// there is an empty <see cref="KBucket"/>, fill it with <see cref="BoundPeer"/>s
         /// in the <see cref="KBucket.ReplacementCache"/>.
--- Libplanet.Net/Swarm.cs
+++ Libplanet.Net/Swarm.cs
@@ -87,7 +87,11 @@ namespace Libplanet.Net
 
             Options = options ?? new SwarmOptions();
             TxCompletion = new TxCompletion<BoundPeer>(BlockChain, GetTxsAsync, BroadcastTxs);
-            RoutingTable = new RoutingTable(Address, Options.TableSize, Options.BucketSize);
+            RoutingTable = new RoutingTable(
+                Address,
+                Options.TableSize,
+                Options.BucketSize,
+                Options.StaticPeers);
 
             // FIXME: after the initialization of NetMQTransport is fully converted to asynchronous
             // code, the portion initializing the swarm in Agent.cs in NineChronicles should be
@@ -1432,9 +1436,19 @@ namespace Libplanet.Net
                 try
                 {
                     await Task.Delay(period, cancellationToken);
-                    await PeerDiscovery.RebuildConnectionAsync(
-                        Kademlia.MaxDepth,
-                        cancellationToken);
+                    if (RoutingTable.SeedPeers.Any())
+                    {
+                        await PeerDiscovery.RebuildConnectionAsync(
+                            RoutingTable.SeedPeers,
+                            1,
+                            cancellationToken);
+                    }
+                    else
+                    {
+                        await PeerDiscovery.RebuildConnectionAsync(
+                            Kademlia.MaxDepth,
+                            cancellationToken);
+                    }
                 }
                 catch (OperationCanceledException e)
                 {
