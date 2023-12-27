b16de5803ed328efb2737589f13906141bfc6da4
Say Cheong <greymistcube@gmail.com>
2023-12-27 13:05:26 +0900

    Added documnetation and changelog

--- Libplanet.Store/Trie/Nodes/ShortNode.cs
+++ Libplanet.Store/Trie/Nodes/ShortNode.cs
@@ -10,12 +10,22 @@ namespace Libplanet.Store.Trie.Nodes
             Key = nibbles.Length > 0
                 ? nibbles
                 : throw new ArgumentException(
-                    $"Given {nameof(nibbles)} cannot be empty", nameof(nibbles));
-            Value = value;
+                    $"Given {nameof(nibbles)} cannot be empty.", nameof(nibbles));
+            Value = value is ShortNode
+                ? throw new ArgumentException(
+                    $"Given {nameof(value)} cannot be a {nameof(ShortNode)}.", nameof(value))
+                : value;
         }
 
         public Nibbles Key { get; }
 
+        /// <summary>
+        /// Represents an <see cref="INode"/> at the end of a <see cref="ShortNode"/>.
+        /// It can be a <see cref="ValueNode"/>, a <see cref="FullNode"/>,
+        /// or a <see cref="HashNode"/> of either a <see cref="ValueNode"/>
+        /// or a <see cref="FullNode"/>.  That is, this can be neither a <see cref="ShortNode"/>
+        /// nor a <see cref="HashNode"/> of a <see cref="ShortNode"/>.
+        /// </summary>
         public INode Value { get; }
 
         /// <inheritdoc cref="IEquatable{T}.Equals"/>
