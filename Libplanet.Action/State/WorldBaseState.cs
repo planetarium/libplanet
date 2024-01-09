cd92599875b35bbd5835439d42a5f1310a45d8d5
Say Cheong <greymistcube@gmail.com>
2024-01-09 17:41:25 +0900

    Clutter removal

--- Libplanet.Action/State/WorldBaseState.cs
+++ Libplanet.Action/State/WorldBaseState.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using Bencodex.Types;
 using Libplanet.Common;
@@ -33,47 +30,21 @@ namespace Libplanet.Action.State
         public bool Legacy { get; private set; }
 
         /// <inheritdoc cref="IWorldState.GetAccount"/>
-        public IAccount GetAccount(Address address) => GetAccounts(new[] { address }).First();
-
-        /// <summary>
-        /// Gets the <see cref="IAccount"/>s of the given <paramref name="addresses"/>.
-        /// </summary>
-        /// <param name="addresses">The <see cref="Address"/>es referring
-        /// the <see cref="IAccount"/>s to get its states.</param>
-        /// <returns>The list of <see cref="IAccountState"/>s of the given
-        /// <paramref name="addresses"/>.</returns>
-        public IReadOnlyList<IAccount> GetAccounts(IReadOnlyList<Address> addresses) =>
-            GetAccountStateRoots(addresses).Select(CreateAccount).ToList();
-
-        private IReadOnlyList<ITrie> GetAccountStateRoots(IReadOnlyList<Address> addresses)
+        public IAccount GetAccount(Address address)
         {
             if (Legacy)
             {
-                return addresses
-                    .Select(GetLegacyTrieOnly)
-                    .ToList();
+                return address.Equals(ReservedAddresses.LegacyAccount)
+                    ? new Account(new AccountState(Trie))
+                    : new Account(new AccountState(_stateStore.GetStateRoot(null)));
             }
             else
             {
-                return Trie
-                    .Get(addresses.Select(ToStateKey).ToList())
-                    .Select(GetTrieFromBencodex)
-                    .ToList();
+                return Trie.Get(ToStateKey(address)) is Binary accountStateRootHash
+                    ? new Account(new AccountState(_stateStore.GetStateRoot(
+                        new HashDigest<SHA256>(accountStateRootHash.ByteArray))))
+                    : new Account(new AccountState(_stateStore.GetStateRoot(null)));
             }
         }
-
-        private ITrie GetLegacyTrieOnly(Address address) =>
-            address == ReservedAddresses.LegacyAccount
-                ? Trie
-                : _stateStore.GetStateRoot(null);
-
-        private ITrie GetTrieFromBencodex(IValue? value) =>
-            value is Binary stateRootHash
-                ? _stateStore.GetStateRoot(
-                    new HashDigest<SHA256>(stateRootHash.ToArray()))
-                : _stateStore.GetStateRoot(null);
-
-        private IAccount CreateAccount(ITrie trie) =>
-            new Account(new AccountState(trie));
     }
 }
