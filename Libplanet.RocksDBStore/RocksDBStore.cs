1140a780b62ee98395ff29ab43d7386c5262e5c0
s2quake <han0210@netsgo.com>
2024-01-22 11:47:29 +0900

    style: Removed '#nullable disable' from the Libplanet.RocksDBStore project

--- Libplanet.RocksDBStore/RocksDBStore.cs
+++ Libplanet.RocksDBStore/RocksDBStore.cs
@@ -1,4 +1,3 @@
-#nullable disable
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -645,7 +644,7 @@ namespace Libplanet.RocksDBStore
 
             _chainDb.Write(batch);
 
-            long bpIndex = GetBlockIndex(branchpoint).Value;
+            long bpIndex = GetBlockIndex(branchpoint)!.Value;
 
             // Do fork from previous chain instead current if it's available and same as current.
             if (GetPreviousChainInfo(sourceChainId) is { } chainInfo &&
@@ -670,7 +669,7 @@ namespace Libplanet.RocksDBStore
         }
 
         /// <inheritdoc/>
-        public override Transaction GetTransaction(TxId txid)
+        public override Transaction? GetTransaction(TxId txid)
         {
             if (_txCache.TryGetValue(txid, out object cachedTx))
             {
@@ -999,7 +998,7 @@ namespace Libplanet.RocksDBStore
             );
 
         /// <inheritdoc cref="BaseStore.GetTxExecution(BlockHash, TxId)"/>
-        public override TxExecution GetTxExecution(BlockHash blockHash, TxId txid)
+        public override TxExecution? GetTxExecution(BlockHash blockHash, TxId txid)
         {
             byte[] key = TxExecutionKey(blockHash, txid);
             if (_txExecutionDb.Get(key) is { } bytes)
@@ -1222,7 +1221,7 @@ namespace Libplanet.RocksDBStore
         }
 
         /// <inheritdoc />
-        public override BlockCommit GetChainBlockCommit(Guid chainId)
+        public override BlockCommit? GetChainBlockCommit(Guid chainId)
         {
             try
             {
@@ -1259,7 +1258,7 @@ namespace Libplanet.RocksDBStore
             }
         }
 
-        public override BlockCommit GetBlockCommit(BlockHash blockHash)
+        public override BlockCommit? GetBlockCommit(BlockHash blockHash)
         {
             _rwBlockCommitLock.EnterReadLock();
 
