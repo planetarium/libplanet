9ea05c4e9e976c0fd7e784c10df763cd295ced03
Say Cheong <greymistcube@gmail.com>
2024-03-19 17:24:19 +0900

    Removed unnecessary methods and properties from IWorldDelta

--- Libplanet.Action/State/IWorldDelta.cs
+++ Libplanet.Action/State/IWorldDelta.cs
@@ -14,14 +14,6 @@ namespace Libplanet.Action.State
         IImmutableDictionary<Address, IAccount> Accounts { get; }
 
         /// <summary>
-        /// A dictionary representing changed account states for each <see cref="Address"/>.
-        /// Elements of this collection are removed when <see cref="CommitAccount"/> called
-        /// by corresponding <see cref="Address"/>.
-        /// </summary>
-        [Pure]
-        IImmutableDictionary<Address, IAccount> Uncommitted { get; }
-
-        /// <summary>
         /// Set account on both of <see cref="Accounts"/> and <see cref="Uncommitted"/>
         /// dictionaries. If <see cref="IAccount"/> already exists on
         /// <paramref name="address"/>, update with new <paramref name="account"/>.
@@ -31,19 +23,5 @@ namespace Libplanet.Action.State
         /// <returns>New <see cref="IWorldDelta"/> that account is properly set.</returns>
         [Pure]
         IWorldDelta SetAccount(Address address, IAccount account);
-
-        /// <summary>
-        /// Remove item from <see cref="Uncommitted"/> dictionary where its key is
-        /// <paramref name="address"/>.
-        /// </summary>
-        /// <param name="address"><see cref="Address"/> of <see cref="IAccount"/> to
-        /// remove from <see cref="Uncommitted"/>.</param>
-        /// <returns>
-        /// New <see cref="IWorldDelta"/> item of <paramref name="address"/> removed from
-        /// <see cref="Uncommitted"/>.
-        /// If corresponding uncommitted does not exist, returns identical one.
-        /// </returns>
-        [Pure]
-        IWorldDelta CommitAccount(Address address);
     }
 }
