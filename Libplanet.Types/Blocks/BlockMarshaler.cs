214b114de3e307737ee3dfc188cc4f0a5f9f79e5
ilgyu <ilgyu.yi@gmail.com>
2024-04-08 10:52:20 +0900

    feat: Add BlockMarshaler.UnmarshalProof()

--- Libplanet.Types/Blocks/BlockMarshaler.cs
+++ Libplanet.Types/Blocks/BlockMarshaler.cs
@@ -240,9 +240,7 @@ namespace Libplanet.Types.Blocks
                 lastCommit: marshaled.ContainsKey(LastCommitKey)
                     ? new BlockCommit(marshaled[LastCommitKey])
                     : (BlockCommit?)null,
-                proof: marshaled.ContainsKey(ProofKey)
-                    ? new Proof(marshaled[ProofKey])
-                    : (Proof?)null);
+                proof: UnmarshalProof(marshaled));
 #pragma warning restore SA1118
         }
 
@@ -312,5 +310,11 @@ namespace Libplanet.Types.Blocks
             IReadOnlyList<Transaction> txs = UnmarshalBlockTransactions(marshaled);
             return new PreEvaluationBlock(header, txs);
         }
+
+        public static Proof? UnmarshalProof(Dictionary marshaled) =>
+            marshaled.ContainsKey(ProofKey)
+                ? new Proof(marshaled[ProofKey])
+                : (Proof?)null;
+
     }
 }
