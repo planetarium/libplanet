c73216b05c16da344aa26da4ff133942842cb2d2
Say Cheong <greymistcube@gmail.com>
2024-04-11 17:44:40 +0900

    Added tests

---
+++ Libplanet.Tests/Store/Trie/TrieMetadataTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Immutable;
+using Bencodex.Types;
+using Libplanet.Store.Trie;
+using Libplanet.Types.Blocks;
+using Xunit;
+using static Libplanet.Tests.TestUtils;
+
+namespace Libplanet.Tests.Store.Trie
+{
+    public class TrieMetadataTest
+    {
+        [Fact]
+        public void CannotCreateWithInvalidVersion()
+        {
+            Assert.Throws<ArgumentException>(
+                () => new TrieMetadata(BlockMetadata.WorldStateProtocolVersion - 1));
+            Assert.Throws<ArgumentException>(
+                () => new TrieMetadata(BlockMetadata.CurrentProtocolVersion + 1));
+        }
+
+        [Fact]
+        public void Bencoded()
+        {
+            var meta = new TrieMetadata(BlockMetadata.WorldStateProtocolVersion);
+            IValue bencoded = meta.Bencoded;
+            var decoded = new TrieMetadata(bencoded);
+            Assert.Equal(meta.Version, decoded.Version);
+        }
+    }
+}
