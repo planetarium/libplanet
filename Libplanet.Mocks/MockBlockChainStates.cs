b11b409562fe401f257382e63c3bb07a45fffbdc
Say Cheong <greymistcube@gmail.com>
2024-05-24 01:26:19 +0900

    Initial implementation of added MockBlockChainStates

---
+++ Libplanet.Mocks/MockBlockChainStates.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Libplanet.Action.State;
+using Libplanet.Common;
+using Libplanet.Store;
+using Libplanet.Store.Trie;
+using Libplanet.Types.Blocks;
+
+namespace Libplanet.Mocks
+{
+    public class MockBlockChainStates : IBlockChainStates
+    {
+        private IStateStore _stateStore;
+
+        private Dictionary<BlockHash, HashDigest<SHA256>> _map;
+
+        public MockBlockChainStates()
+        {
+            _stateStore = new TrieStateStore(new MemoryKeyValueStore());
+            _map = new Dictionary<BlockHash, HashDigest<SHA256>>();
+        }
+
+        public IStateStore StateStore => _stateStore;
+
+        public void AttachBlockHashToStateRootHash(
+            BlockHash blockHash,
+            HashDigest<SHA256> stateRootHash)
+        {
+            if (_map.ContainsKey(blockHash))
+            {
+                throw new ArgumentException(
+                    $"Already contains block hash {blockHash} associated to " +
+                    $"state root hash {_map[blockHash]}.",
+                    nameof(blockHash));
+            }
+            else if (!_stateStore.GetStateRoot(stateRootHash).Recorded)
+            {
+                throw new ArgumentException(
+                    $"No state root for given state root hash {stateRootHash} found.",
+                    nameof(stateRootHash));
+            }
+
+            _map[blockHash] = stateRootHash;
+        }
+
+        /// <inheritdoc cref="IBlockChainStates.GetWorldState(BlockHash?)"/>
+        public IWorldState GetWorldState(BlockHash? offset)
+        {
+            if (offset is { } blockHash)
+            {
+                if (_map.ContainsKey(blockHash))
+                {
+                    return GetWorldState(_map[blockHash]);
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        $"No state root associated with given block hash {blockHash} found.",
+                        nameof(blockHash));
+                }
+            }
+            else
+            {
+                return GetWorldState((HashDigest<SHA256>?)null);
+            }
+        }
+
+        /// <inheritdoc cref="IBlockChainStates.GetWorldState(HashDigest{SHA256}?)"/>
+        public IWorldState GetWorldState(HashDigest<SHA256>? stateRootHash)
+        {
+            ITrie trie = _stateStore.GetStateRoot(stateRootHash);
+            return trie.Recorded
+                ? new WorldBaseState(trie, _stateStore)
+                : throw new ArgumentException(
+                    $"Could not find state root {stateRootHash} in {nameof(IStateStore)}.",
+                    nameof(stateRootHash));
+        }
+    }
+}
