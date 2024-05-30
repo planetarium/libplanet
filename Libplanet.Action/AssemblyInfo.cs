d10613e3a1b49bc92c956e18bab16606d2f35f2f
Say Cheong <greymistcube@gmail.com>
2024-05-30 11:49:48 +0900

    Changed to automatically prepare migration during Swarm instantiation

--- Libplanet.Action/AssemblyInfo.cs
+++ Libplanet.Action/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Libplanet.Action.Tests")]
 [assembly: InternalsVisibleTo("Libplanet.Explorer.Tests")]
 [assembly: InternalsVisibleTo("Libplanet.Mocks")]
+[assembly: InternalsVisibleTo("Libplanet.Net")]
