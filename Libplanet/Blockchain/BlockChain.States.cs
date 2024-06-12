a0723965ec41b48c1ba1a4b9ff4e644e17d8fb80
Say Cheong <greymistcube@gmail.com>
2024-06-12 13:21:47 +0900

    Restore documentation

--- Libplanet/Blockchain/BlockChain.States.cs
+++ Libplanet/Blockchain/BlockChain.States.cs
@@ -2,6 +2,7 @@ using System;
 using System.Security.Cryptography;
 using Libplanet.Action.State;
 using Libplanet.Common;
+using Libplanet.Store;
 using Libplanet.Types.Blocks;
 
 namespace Libplanet.Blockchain
@@ -28,6 +29,20 @@ namespace Libplanet.Blockchain
         /// <returns>The next world state.  If it does not exist, returns null.</returns>
         public IWorldState? GetNextWorldState() => GetNextWorldState(Tip.Hash);
 
+        /// <summary>
+        /// Returns the next <see cref="IWorldState"/> in the BlockChain
+        /// at <paramref name="offset"/>.
+        /// </summary>
+        /// <param name="offset">The <see cref="BlockHash"/> of the <see cref="Block"/> to create
+        /// for which to create an <see cref="IWorldState"/>.</param>
+        /// <returns>
+        /// The next <see cref="IWorldState"/> at <paramref name="offset"/>.
+        /// Returns <see langword="null"/> if next state root hash does not exists.
+        /// </returns>
+        /// <exception cref="ArgumentException">Thrown when next state root hash exists,
+        /// but corresponding state root is not found in the <see cref="IStateStore"/>.
+        /// </exception>
+        /// <seealso cref="IWorldState"/>
         public IWorldState? GetNextWorldState(BlockHash offset)
         {
             var nextSrh = Store.GetNextStateRootHash(offset);
