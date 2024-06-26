d0e35b042d12a08fa347181b4f5978d13d3cb3ee
s2quake <han0210@netsgo.com>
2024-06-26 13:09:38 +0900

    chore: Fix ci scripts.

--- scripts/determine-version.js
+++ scripts/determine-version.js
@@ -76,9 +76,7 @@ function getScheduledJobDate() {
 async function main() {
   const csprojPath = path.join(
     path.dirname(__dirname),
-    "src",
-    "Libplanet",
-    "Libplanet.csproj",
+    "Directory.Build.props",
   );
   const versionPrefix = await readVersionPrefix(csprojPath);
   const scheduledJobDate = getScheduledJobDate();
