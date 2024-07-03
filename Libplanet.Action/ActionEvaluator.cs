fc6753889dd22b752b6c4d96ab7e3fd1d1f88ce8
Chanhyuck Ko <limeelbee@gmail.com>
2024-07-03 18:42:32 +0900

    feat: remove feecollector

--- Libplanet.Action/ActionEvaluator.cs
+++ Libplanet.Action/ActionEvaluator.cs
@@ -278,7 +278,6 @@ namespace Libplanet.Action
             IActionContext inputContext = context;
             IWorld state = inputContext.PreviousState;
             Exception? exc = null;
-            IFeeCollector feeCollector = new FeeCollector(context, tx?.MaxGasPrice);
 
             IActionContext CreateActionContext(IWorld newPrevState)
             {
@@ -302,9 +301,7 @@ namespace Libplanet.Action
                 Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
                 AccountMetrics.Initialize();
-                state = feeCollector.Mortgage(state);
                 context = CreateActionContext(state);
-                feeCollector = feeCollector.Next(context);
                 state = action.Execute(context);
                 logger?
                     .ForContext("Tag", "Metric")
@@ -366,8 +363,6 @@ namespace Libplanet.Action
                     e);
             }
 
-            state = feeCollector.Refund(state);
-            state = feeCollector.Reward(state);
             state = stateStore.CommitWorld(state);
 
             if (!state.Trie.Recorded)
