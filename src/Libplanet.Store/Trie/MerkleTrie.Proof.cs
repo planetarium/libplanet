9538b89b682614a87e623e42be247048206ef579
Say Cheong <greymistcube@gmail.com>
2024-07-10 12:59:42 +0900

    Added initial implementation of proof extraction

--- src/Libplanet.Store/Trie/MerkleTrie.Proof.cs
+++ src/Libplanet.Store/Trie/MerkleTrie.Proof.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Security.Cryptography;
 using Bencodex.Types;
 using Libplanet.Common;
@@ -9,18 +10,6 @@ namespace Libplanet.Store.Trie
 {
     public partial class MerkleTrie
     {
-        public IReadOnlyList<INode> GetProof(KeyBytes key)
-        {
-            if (!Recorded)
-            {
-                throw new InvalidOperationException(
-                    $"A proof can only be retrieved from a recorded {nameof(ITrie)}");
-            }
-
-            PathCursor cursor = new PathCursor(key);
-            throw new NotImplementedException();
-        }
-
         public static bool IsValidProof(
             HashDigest<SHA256> stateRootHash,
             IReadOnlyList<INode> proof,
@@ -33,10 +22,10 @@ namespace Libplanet.Store.Trie
             {
                 INode proofNode = proof[i];
                 bool first = i == 0;
-                bool last = (i == (proof.Count - 1));
+                bool last = i == proof.Count - 1;
 
                 // A proof node cannot be a HashNode.
-                if (proofNode is HashNode hashNode)
+                if (proofNode is HashNode)
                 {
                     return false;
                 }
@@ -72,6 +61,77 @@ namespace Libplanet.Store.Trie
             }
 
             return false;
+        }
+
+        public IReadOnlyList<INode> GetProof(KeyBytes key, IValue value)
+        {
+            if (!Recorded)
+            {
+                throw new InvalidOperationException(
+                    $"A proof can only be retrieved from a recorded {nameof(ITrie)}");
+            }
+
+            INode root = Root ??
+                throw new InvalidOperationException(
+                    $"A proof can only be retrieved from a non-null {nameof(Root)}");
+
+            INode hashNode = root is HashNode h
+                ? h
+                : throw new InvalidOperationException(
+                    $"Encountered an unexpected node type {root.GetType()}");
+
+            List<INode> proof = new List<INode>();
+            PathCursor cursor = new PathCursor(key);
+
+            while (hashNode is HashNode currentHashNode)
+            {
+                INode unhashedNode = UnhashNode(currentHashNode);
+                proof.Add(unhashedNode);
+                var resolved = ResolveToNextCandidateNode(unhashedNode, cursor);
+
+                if (resolved is { } r)
+                {
+                    if (r.NextNode is HashNode)
+                    {
+                        hashNode = r.NextNode;
+                        cursor = r.NextCursor;
+                        continue;
+                    }
+                    else if (r.NextNode is ValueNode nextValueNode)
+                    {
+                        if (r.NextCursor.Offset == r.NextCursor.Length)
+                        {
+                            return nextValueNode.Value.Equals(value)
+                                ? proof.ToImmutableList()
+                                : throw new ArgumentException(
+                                    $"Given value {value} does not match " +
+                                    $"the actual value {nextValueNode.Value}" +
+                                    $"found at given key {key}",
+                                    nameof(value));
+                        }
+                        else
+                        {
+                            throw new ArgumentException(
+                                $"Given key {key} could not be resolved.",
+                                nameof(key));
+                        }
+                    }
+                    else
+                    {
+                        // NOTE: Should not be thrown.
+                        throw new ArgumentException("Failed to get a proof.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        $"Given key {key} could not be resolved.",
+                        nameof(key));
+                }
+            }
+
+            // NOTE: Should not be thrown.
+            throw new ArgumentException("Something went wrong.");
         }
 
         private static bool CheckProofNodeHash(
@@ -196,6 +256,69 @@ namespace Libplanet.Store.Trie
                         if (nextNode is INode n)
                         {
                             return ResolveToHashNode(n, cursor.Next(1));
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+
+                default:
+                    throw new ArgumentException(
+                        $"Encountered an unexpected node type {node.GetType()}");
+            }
+        }
+
+        private static (INode NextNode, PathCursor NextCursor)? ResolveToNextCandidateNode(
+            INode node,
+            PathCursor cursor)
+        {
+            switch (node)
+            {
+                // This operates under an assumption that the initial call to this method
+                // will not be called with a HashNode.
+                case HashNode hashNode:
+                    return (hashNode, cursor);
+
+                case ValueNode valueNode:
+                    if (cursor.Offset == cursor.Length)
+                    {
+                        return (valueNode, cursor);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+
+                case ShortNode shortNode:
+                    if (cursor.RemainingNibblesStartWith(shortNode.Key))
+                    {
+                        return ResolveToNextCandidateNode(
+                            shortNode.Value, cursor.Next(shortNode.Key.Length));
+                    }
+                    else
+                    {
+                        return null;
+                    }
+
+                case FullNode fullNode:
+                    if (cursor.Offset == cursor.Length)
+                    {
+                        if (fullNode.Value is INode n)
+                        {
+                            return (n, cursor);
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                    else
+                    {
+                        INode? nextNode = fullNode.Children[cursor.NextNibble];
+                        if (nextNode is INode n)
+                        {
+                            return ResolveToNextCandidateNode(n, cursor.Next(1));
                         }
                         else
                         {
