5dd412e878a95494071a8df642dc48f040b38ded
Say Cheong <greymistcube@gmail.com>
2024-07-30 21:43:04 +0900

    Refactored BlockLocator.Create()

--- test/Libplanet.Tests/Blockchain/BlockLocatorTest.cs
+++ test/Libplanet.Tests/Blockchain/BlockLocatorTest.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Libplanet.Blockchain;
 using Libplanet.Types.Blocks;
 using Xunit;
@@ -19,80 +18,16 @@ namespace Libplanet.Tests.Blockchain
         [Fact]
         public void Create()
         {
-            // Generate fixture block hashes looks like 0x00...00 0x00...01, 0x00...02, and so on,
-            // for the sake of easier debugging.
-            const int count = 0x10;
-            List<long> indices = Enumerable.Range(0, count).Select(i => (long)i).ToList();
-            Func<long, BlockHash> indexToBlockHash = i =>
-            {
-                byte[] bytes = GetBigEndianBytes(i);
-                var buffer = new byte[32];
-                bytes.CopyTo(buffer, buffer.Length - bytes.Length);
-                return new BlockHash(buffer);
-            };
-            List<BlockHash> hashes = indices.Select(i => indexToBlockHash(i)).ToList();
+            BlockHash genesisHash = new BlockHash(TestUtils.GetRandomBytes(BlockHash.Size));
+            BlockHash tipHash = genesisHash;
+            BlockLocator locator = BlockLocator.Create(genesisHash, tipHash);
+            Assert.Equal(genesisHash, Assert.Single(locator));
 
-            Assert.Throws<ArgumentOutOfRangeException>(() =>
-                BlockLocator.Create(
-                    startIndex: -1,
-                    indexToBlockHash: i => (BlockHash?)indexToBlockHash(i)));
-            Assert.Throws<ArgumentException>(() =>
-                BlockLocator.Create(
-                    startIndex: 0,
-                    indexToBlockHash: i => i == 0 ? null : (BlockHash?)indexToBlockHash(i)));
-
-            var locator = BlockLocator.Create(
-                startIndex: hashes.Count - 1,
-                indexToBlockHash: i => (BlockHash?)indexToBlockHash(i));
-            var expected = new BlockHash[]
-            {
-                hashes[0xf],
-                hashes[0x0],
-            };
-
-            Assert.Equal(expected, locator);
-
-            locator = BlockLocator.Create(
-                startIndex: hashes.Count - 3,
-                indexToBlockHash: i => (BlockHash?)indexToBlockHash(i));
-            expected = new BlockHash[]
-            {
-                hashes[0xd],
-                hashes[0x0],
-            };
-
-            Assert.Equal(expected, locator);
-
-            locator = BlockLocator.Create(
-                startIndex: 0,
-                indexToBlockHash: i => (BlockHash?)indexToBlockHash(i));
-            expected = new BlockHash[]
-            {
-                hashes[0x0],
-            };
-
-            Assert.Equal(expected, locator);
-
-            locator = BlockLocator.Create(
-                startIndex: count,
-                indexToBlockHash: i => i >= count ? null : (BlockHash?)indexToBlockHash(i));
-            expected = new BlockHash[]
-            {
-                hashes[0x0],
-            };
-
-            Assert.Equal(expected, locator);
-        }
-
-        private static byte[] GetBigEndianBytes(long value)
-        {
-            byte[] bytes = BitConverter.GetBytes(value);
-            if (BitConverter.IsLittleEndian)
-            {
-                Array.Reverse(bytes);
-            }
-
-            return bytes;
+            tipHash = new BlockHash(TestUtils.GetRandomBytes(BlockHash.Size));
+            locator = BlockLocator.Create(genesisHash, tipHash);
+            Assert.Equal(
+                new BlockHash[] { tipHash, genesisHash },
+                locator);
         }
     }
 }
