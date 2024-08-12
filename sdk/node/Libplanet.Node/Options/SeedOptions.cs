39882afde1b0087a78ac495ddb52612bf9eb405f
s2quake <han0210@netsgo.com>
2024-08-12 17:53:01 +0900

    feat: Geneate and add schema file

--- sdk/node/Libplanet.Node/Options/SeedOptions.cs
+++ sdk/node/Libplanet.Node/Options/SeedOptions.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Libplanet.Node.DataAnnotations;
 using Libplanet.Node.DependencyInjection;
@@ -12,17 +13,22 @@ public sealed class SeedOptions : OptionsBase<SeedOptions>
     public const string ConsensusSeed = nameof(ConsensusSeed);
 
     [PrivateKey]
+    [Description("The private key of the seed node.")]
     public string PrivateKey { get; set; } = string.Empty;
 
     [DnsEndPoint]
+    [Description("The endpoint of the seed node.")]
     public string EndPoint { get; set; } = string.Empty;
 
     [Range(0, int.MaxValue)]
+    [Description("The interval to refresh the peer list.")]
     public int RefreshInterval { get; set; } = 5;
 
     [Range(0, int.MaxValue)]
+    [Description("The lifetime of a peer.")]
     public int PeerLifetime { get; set; } = 120;
 
     [Range(0, int.MaxValue)]
+    [Description("The timeout for a ping.")]
     public int PingTimeout { get; set; } = 5;
 }
