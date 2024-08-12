864a4e44c55c14dcee758bb4f52b13435f98ebd9
s2quake <han0210@netsgo.com>
2024-08-12 17:52:44 +0900

    feat: Add json schema builder for appsettings.json

--- sdk/node/Libplanet.Node.Executable/Program.cs
+++ sdk/node/Libplanet.Node.Executable/Program.cs
@@ -1,5 +1,6 @@
 using Libplanet.Node.API.Services;
 using Libplanet.Node.Extensions;
+using Libplanet.Node.Options.Schema;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 SynchronizationContext.SetSynchronizationContext(new());
@@ -13,7 +14,7 @@ if (builder.Environment.IsDevelopment())
     {
         // Setup a HTTP/2 endpoint without TLS.
         options.ListenLocalhost(5259, o => o.Protocols =
-            HttpProtocols.Http2);
+            HttpProtocols.Http1AndHttp2);
     });
 }
 
@@ -33,10 +34,12 @@ var handlerMessage = """
     Communication with gRPC endpoints must be made through a gRPC client. To learn how to
     create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
     """;
+var schema = await OptionsSchemaBuilder.GetSchemaAsync(default);
 
 // Configure the HTTP request pipeline.
 app.MapGrpcServiceFromDomain(libplanetBuilder.Scopes);
 app.MapGet("/", () => handlerMessage);
+app.MapGet("/schema", () => schema);
 
 if (builder.Environment.IsDevelopment())
 {
