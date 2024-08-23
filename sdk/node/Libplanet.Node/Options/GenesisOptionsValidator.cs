50a6c6b8c9672b7df4bdaa424043ff4f06dcf093
s2quake <han0210@netsgo.com>
2024-08-23 14:24:59 +0900

    feat: Add GenesisBlockPath option to GenesisOptions

---
+++ sdk/node/Libplanet.Node/Options/GenesisOptionsValidator.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.Options;
+
+namespace Libplanet.Node.Options;
+
+internal sealed class GenesisOptionsValidator : OptionsValidatorBase<GenesisOptions>
+{
+    protected override void OnValidate(string? name, GenesisOptions options)
+    {
+        if (options.GenesisBlockPath != string.Empty)
+        {
+            if (options.GenesisKey != string.Empty)
+            {
+                var message = $"{nameof(options.GenesisKey)} cannot be used with " +
+                              $"{nameof(options.GenesisBlockPath)}.";
+                throw new OptionsValidationException(
+                    optionsName: name ?? string.Empty,
+                    optionsType: typeof(GenesisOptions),
+                    failureMessages: [message]);
+            }
+
+            if (options.Validators.Length > 0)
+            {
+                var message = $"{nameof(options.Validators)} cannot be used with " +
+                              $"{nameof(options.GenesisBlockPath)}.";
+                throw new OptionsValidationException(
+                optionsName: name ?? string.Empty,
+                optionsType: typeof(GenesisOptions),
+                failureMessages: [message]);
+            }
+
+            if (!Uri.TryCreate(options.GenesisBlockPath, UriKind.Absolute, out _)
+                && !File.Exists(options.GenesisBlockPath))
+            {
+                var message = $"{nameof(options.GenesisBlockPath)} must be a Uri or a existing " +
+                              $"file path.";
+                throw new OptionsValidationException(
+                    optionsName: name ?? string.Empty,
+                    optionsType: typeof(GenesisOptions),
+                    failureMessages: [message]);
+            }
+        }
+    }
+}
