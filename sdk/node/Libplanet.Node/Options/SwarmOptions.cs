262ef041f42be9c379d8bac7e7dde309559adfea
s2quake <han0210@netsgo.com>
2024-08-30 15:37:23 +0900

    feat: Add AppProtocolOptionsBase for common properties like PrivateKey and APV

--- sdk/node/Libplanet.Node/Options/SwarmOptions.cs
+++ sdk/node/Libplanet.Node/Options/SwarmOptions.cs
@@ -4,22 +4,18 @@ using Libplanet.Node.DataAnnotations;
 namespace Libplanet.Node.Options;
 
 [Options(Position)]
-public sealed class SwarmOptions : OptionsBase<SwarmOptions>, IEnabledOptions
+public sealed class SwarmOptions : AppProtocolOptionsBase<SwarmOptions>, IEnabledOptions
 {
     public const string Position = "Swarm";
 
     public bool IsEnabled { get; set; }
 
     [PrivateKey]
-    [Description("The private key of the node.")]
+    [Description("The private key of Swarm.")]
     public string PrivateKey { get; set; } = string.Empty;
 
     [DnsEndPoint]
     public string EndPoint { get; set; } = string.Empty;
-
-    [AppProtocolVersion]
-    [Description("The version of the application protocol.")]
-    public string AppProtocolVersion { get; set; } = string.Empty;
 
     [BoundPeer]
     [Description("The endpoint of the node to block sync.")]
--- sdk/node/Libplanet.Node/Options/ValidatorOptions.cs
+++ sdk/node/Libplanet.Node/Options/ValidatorOptions.cs
@@ -4,12 +4,15 @@ using Libplanet.Node.DataAnnotations;
 namespace Libplanet.Node.Options;
 
 [Options(Position)]
-public sealed class ValidatorOptions : OptionsBase<ValidatorOptions>, IEnabledOptions
+public sealed class ValidatorOptions : AppProtocolOptionsBase<ValidatorOptions>, IEnabledOptions
 {
     public const string Position = "Validator";
 
-    [DefaultValue(true)]
     public bool IsEnabled { get; set; }
+
+    [PrivateKey]
+    [Description("The private key of Validator.")]
+    public string PrivateKey { get; set; } = string.Empty;
 
     [DnsEndPoint]
     public string EndPoint { get; set; } = string.Empty;
