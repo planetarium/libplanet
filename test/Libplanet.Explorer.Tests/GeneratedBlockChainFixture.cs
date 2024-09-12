6fb2d7fa0b2d4b9bd6ca6e0aa74304b1cb403ab0
Say Cheong <greymistcube@gmail.com>
2024-09-12 20:38:22 +0900

    Removed Initialize in Fixture

--- test/Libplanet.Explorer.Tests/GeneratedBlockChainFixture.cs
+++ test/Libplanet.Explorer.Tests/GeneratedBlockChainFixture.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Numerics;
@@ -16,6 +17,7 @@ using Libplanet.Types.Evidence;
 using Libplanet.Types.Tx;
 using Libplanet.Store;
 using Libplanet.Store.Trie;
+using Libplanet.Action.State;
 
 namespace Libplanet.Explorer.Tests;
 
@@ -75,23 +77,15 @@ public class GeneratedBlockChainFixture
             policy.PolicyActionsRegistry,
             stateStore,
             TypedActionLoader.Create(typeof(SimpleAction).Assembly, typeof(SimpleAction)));
-        Block genesisBlock = BlockChain.ProposeGenesisBlock(
-            transactions: PrivateKeys
-                .OrderBy(pk => pk.Address.ToHex())
-                .Select(
-                    (pk, i) => Transaction.Create(
-                        nonce: i,
-                        privateKey: privateKey,
-                        genesisHash: null,
-                        actions: new IAction[]
-                            {
-                                new Initialize(
-                                    new ValidatorSet(
-                                        ImmutableList<Validator>.Empty.Add(
-                                            new Validator(pk.PublicKey, 1)).ToList()),
-                                    ImmutableDictionary.Create<Address, IValue>())
-                            }.ToPlainValues()))
-                .ToImmutableList());
+        var initialWorld = new Dictionary<Address, Dictionary<Address, IValue>>();
+        var validatorSet = new ValidatorSet(
+            PrivateKeys.Select(pk => new Validator(pk.PublicKey, 1)).ToList());
+        initialWorld[ReservedAddresses.ValidatorSetAccount] = new Dictionary<Address, IValue>()
+        {
+            { ValidatorSetAccount.ValidatorSetAddress, validatorSet.Bencoded }
+        };
+        var initialStaterootHash = stateStore.CommitWorld(initialWorld);
+        Block genesisBlock = BlockChain.ProposeGenesisBlock(stateRootHash: initialStaterootHash);
         Chain = BlockChain.Create(
             policy,
             new VolatileStagePolicy(),
