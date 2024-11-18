0c1882d35954efbd67438033509052ddf581b5aa
Say Cheong <greymistcube@gmail.com>
2024-11-18 14:07:16 +0900

    AsyncDelegate updated

--- src/Libplanet.Net/AsyncDelegate.cs
+++ src/Libplanet.Net/AsyncDelegate.cs
@@ -1,20 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Channels;
 using System.Threading.Tasks;
+using Libplanet.Net.Messages;
 
 namespace Libplanet.Net
 {
-    public class AsyncDelegate<T>
+    public class AsyncDelegate
     {
-        private IEnumerable<Func<T, Task>> _functions;
+        private IEnumerable<Func<Message, Channel<MessageContent>, Task>> _functions;
 
         public AsyncDelegate()
         {
-            _functions = new List<Func<T, Task>>();
+            _functions = new List<Func<Message, Channel<MessageContent>, Task>>();
         }
 
-        public void Register(Func<T, Task> func)
+        public void Register(Func<Message, Channel<MessageContent>, Task> func)
         {
 #pragma warning disable PC002
             // Usage of a .NET Standard API that isn’t available on the .NET Framework 4.6.1
@@ -26,14 +28,14 @@ namespace Libplanet.Net
 #pragma warning restore PC002
         }
 
-        public void Unregister(Func<T, Task> func)
+        public void Unregister(Func<Message, Channel<MessageContent>, Task> func)
         {
             _functions = _functions.Where(f => !f.Equals(func));
         }
 
-        public async Task InvokeAsync(T arg)
+        public async Task InvokeAsync(Message message, Channel<MessageContent> channel)
         {
-            IEnumerable<Task> tasks = _functions.Select(f => f(arg));
+            IEnumerable<Task> tasks = _functions.Select(f => f(message, channel));
             await Task.WhenAll(tasks).ConfigureAwait(false);
         }
     }
